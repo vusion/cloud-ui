f56780d23698723a9a1d07bf671c93826b9729ac
Rainfore <rainforest92@126.com>
2017-04-18 12:06:34 +0800

    :sparkles: Use RouterItem mixin in <u-sidebar-item>
    Signed-off-by: Rainfore <rainforest92@126.com>

--- src/u-sidebar-item.vue/index.js
+++ src/u-sidebar-item.vue/index.js
@@ -1,36 +1,9 @@
 import Base from 'u-base.vue';
+import RouterItem from 'u-base.vue/mixins';
 
 const SidebarItem = Base.extend({
     name: 'u-sidebar-item',
-    props: {
-        to: [String, Object],
-        exact: Boolean,
-        append: Boolean,
-        replace: Boolean,
-    },
-    computed: {
-        active() {
-            if (!this.$route || this.to === undefined)
-                return;
-
-            const current = this.$route;
-            const route = this.$router.resolve(this.to, current, this.append).route;
-
-            return this.exact ? route.path === current.path : current.path.startsWith(route.path);
-        },
-    },
-    methods: {
-        onClick(e) {
-            if (!this.$route || this.to === undefined)
-                return;
-
-            const router = this.$router;
-
-            router.push(this.to);
-
-            this.$emit('click', e);
-        },
-    },
+    mixins: [RouterItem],
 });
 
 export default SidebarItem;
