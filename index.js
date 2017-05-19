68cc41268f9e8cf9dcbc2b987daa2edc3d663549
haoxutong <1255965516@qq.com>
2017-05-19 12:56:22 +0800

    :lipstick: Change style name in <u-line-chart>

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ import DatePicker from './src/u-datepicker.vue';
 import TimePicker from './src/u-timepicker.vue';
 import Chart from './src/u-chart.vue';
 import PieChart from './src/u-piechart.vue';
-import LineChart from './src/u-linechart.vue';
+import LineChart from './src/u-line-chart.vue';
 import BarChart from './src/u-barchart.vue';
 import DateTimePicker from './src/u-datetimepicker.vue';
 
