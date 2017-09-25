ff6e67967c97c90c7fdb90f8ed2ee475ae021b58
郝绪彤 <1255965516@qq.com>
2017-09-25 16:17:49 +0800

    Updates

---
+++ theme-dark/chunk.18.5ac954660421deca.js
@@ -0,0 +1,127 @@
+webpackJsonp([18],{
+
+/***/ 650:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(741);
+
+/***/ }),
+
+/***/ 741:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_template_compiler_index_id_data_v_46255faa_hasScoped_false_C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_selector_type_template_index_0_node_modules_vue_markdown_loader_lib_markdown_compiler_js_raw_README_md__ = __webpack_require__(742);
+var normalizeComponent = __webpack_require__(7)
+/* script */
+var __vue_script__ = null
+/* template */
+
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __WEBPACK_IMPORTED_MODULE_0__C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_template_compiler_index_id_data_v_46255faa_hasScoped_false_C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_selector_type_template_index_0_node_modules_vue_markdown_loader_lib_markdown_compiler_js_raw_README_md__["a" /* default */],
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+
+/***/ 742:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('u-article', [_c('h1', [_vm._v("弹窗（PieChart）")]), _vm._v(" "), _c('h2', [_vm._v("示例")]), _vm._v(" "), _c('h3', [_vm._v("基本形式")]), _vm._v(" "), _c('div', {
+    staticClass: "u-example"
+  }, [_c('u-pie-chart', {
+    attrs: {
+      "border": "",
+      "legend": "",
+      "title": "每星期访问量",
+      "data": [{
+        'percent': 25,
+        'key': '1',
+        'name': 'selector1'
+      }, {
+        'percent': 25,
+        'key': '2',
+        'name': 'selector2'
+      }, {
+        'percent': 20,
+        'key': '3',
+        'name': 'selector3'
+      }, {
+        'percent': 30,
+        'key': '4',
+        'name': 'selector4'
+      }]
+    }
+  })], 1), _vm._v(" "), _c('pre', {
+    pre: true,
+    attrs: {
+      "class": "hljs lang-html"
+    }
+  }, [_c('code', {
+    attrs: {
+      "v-pre": ""
+    }
+  }, [_c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-pie-chart")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("border")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("legend")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("title")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"每星期访问量\"")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v(":data")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"[{ 'percent': 25, 'key': '1', 'name': 'selector1' }, { 'percent': 25, 'key': '2', 'name': 'selector2' }, { 'percent': 20, 'key': '3', 'name': 'selector3' }, { 'percent': 30, 'key': '4', 'name': 'selector4' }]\"")]), _vm._v(">")]), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-pie-chart")]), _vm._v(">")]), _vm._v("\n")])])])
+}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+
+/***/ })
+
+});
---
+++ theme-seagreen/chunk.26.75c13856338f3cab.js
@@ -0,0 +1,258 @@
+webpackJsonp([26],{
+
+/***/ 603:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(649);
+
+/***/ }),
+
+/***/ 649:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_template_compiler_index_id_data_v_b6f775aa_hasScoped_false_C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_selector_type_template_index_0_node_modules_vue_markdown_loader_lib_markdown_compiler_js_raw_README_md__ = __webpack_require__(650);
+var normalizeComponent = __webpack_require__(7)
+/* script */
+var __vue_script__ = null
+/* template */
+
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __WEBPACK_IMPORTED_MODULE_0__C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_template_compiler_index_id_data_v_b6f775aa_hasScoped_false_C_Users_hzhaoxutong_AppData_Roaming_npm_node_modules_vusion_cli_node_modules_vusion_vue_loader_lib_selector_type_template_index_0_node_modules_vue_markdown_loader_lib_markdown_compiler_js_raw_README_md__["a" /* default */],
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+
+/* harmony default export */ __webpack_exports__["default"] = (Component.exports);
+
+
+/***/ }),
+
+/***/ 650:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('u-article', [_c('h1', [_vm._v("链接（Link）")]), _vm._v(" "), _c('h2', [_vm._v("示例")]), _vm._v(" "), _c('h3', [_vm._v("基本形式")]), _vm._v(" "), _c('div', {
+    staticClass: "u-example"
+  }, [_c('u-link', {
+    attrs: {
+      "href": "#"
+    }
+  }, [_vm._v("链接")]), _vm._v(" "), _c('u-link', {
+    attrs: {
+      "href": "#"
+    }
+  }, [_vm._v("查看详情")])], 1), _vm._v(" "), _c('pre', {
+    pre: true,
+    attrs: {
+      "class": "hljs lang-html"
+    }
+  }, [_c('code', {
+    attrs: {
+      "v-pre": ""
+    }
+  }, [_c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("href")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"#\"")]), _vm._v(">")]), _vm._v("链接"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(">")]), _vm._v("&nbsp;"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("href")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"#\"")]), _vm._v(">")]), _vm._v("查看详情"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(">")]), _vm._v("\n")])]), _vm._v(" "), _c('h3', [_vm._v("禁用")]), _vm._v(" "), _c('div', {
+    staticClass: "u-example"
+  }, [_c('u-link', {
+    attrs: {
+      "href": "#",
+      "disabled": ""
+    }
+  }, [_vm._v("Link")])], 1), _vm._v(" "), _c('pre', {
+    pre: true,
+    attrs: {
+      "class": "hljs lang-html"
+    }
+  }, [_c('code', {
+    attrs: {
+      "v-pre": ""
+    }
+  }, [_c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("href")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"#\"")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("disabled")]), _vm._v(">")]), _vm._v("Link"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(">")]), _vm._v("\n")])]), _vm._v(" "), _c('h3', [_c('code', {
+    pre: true
+  }, [_vm._v("href")]), _vm._v(" vs "), _c('code', {
+    pre: true
+  }, [_vm._v("to")])]), _vm._v(" "), _c('div', {
+    staticClass: "u-example"
+  }, [_c('u-link', {
+    attrs: {
+      "href": "https://github.com/vusion/vusion",
+      "target": "_blank"
+    }
+  }, [_vm._v("href")]), _vm._v(" \n"), _c('u-link', {
+    attrs: {
+      "to": "/some/router/path"
+    }
+  }, [_vm._v("to")])], 1), _vm._v(" "), _c('pre', {
+    pre: true,
+    attrs: {
+      "class": "hljs lang-html"
+    }
+  }, [_c('code', {
+    attrs: {
+      "v-pre": ""
+    }
+  }, [_c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("href")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"https://github.com/vusion/vusion\"")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("target")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"_blank\"")]), _vm._v(">")]), _vm._v("href"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(">")]), _vm._v("&nbsp;\n"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("<"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(" "), _c('span', {
+    attrs: {
+      "class": "hljs-attr"
+    }
+  }, [_vm._v("to")]), _vm._v("="), _c('span', {
+    attrs: {
+      "class": "hljs-string"
+    }
+  }, [_vm._v("\"/some/router/path\"")]), _vm._v(">")]), _vm._v("to"), _c('span', {
+    attrs: {
+      "class": "hljs-tag"
+    }
+  }, [_vm._v("</"), _c('span', {
+    attrs: {
+      "class": "hljs-name"
+    }
+  }, [_vm._v("u-link")]), _vm._v(">")]), _vm._v("\n")])]), _vm._v(" "), _c('h2', [_vm._v("API")]), _vm._v(" "), _c('h3', [_vm._v("Attrs/Props")]), _vm._v(" "), _c('table', [_c('thead', [_c('tr', [_c('th', [_vm._v("Attr/Prop")]), _vm._v(" "), _c('th', [_vm._v("Type")]), _vm._v(" "), _c('th', [_vm._v("Default")]), _vm._v(" "), _c('th', [_vm._v("Description")])])]), _vm._v(" "), _c('tbody', [_c('tr', [_c('td', [_vm._v("href")]), _vm._v(" "), _c('td', [_vm._v("String")]), _vm._v(" "), _c('td'), _vm._v(" "), _c('td', [_c('code', {
+    pre: true
+  }, [_vm._v("href")]), _vm._v(" property of link")])]), _vm._v(" "), _c('tr', [_c('td', [_vm._v("target")]), _vm._v(" "), _c('td', [_vm._v("String")]), _vm._v(" "), _c('td'), _vm._v(" "), _c('td', [_vm._v("(native property)")])]), _vm._v(" "), _c('tr', [_c('td', [_vm._v("to")]), _vm._v(" "), _c('td', [_vm._v("String | Object")]), _vm._v(" "), _c('td'), _vm._v(" "), _c('td', [_c('code', {
+    pre: true
+  }, [_vm._v("to")]), _vm._v(" property of "), _c('code', {
+    pre: true
+  }, [_vm._v("vue-router")])])]), _vm._v(" "), _c('tr', [_c('td', [_vm._v("replace")]), _vm._v(" "), _c('td', [_vm._v("Boolean")]), _vm._v(" "), _c('td', [_c('code', {
+    pre: true
+  }, [_vm._v("false")])]), _vm._v(" "), _c('td')]), _vm._v(" "), _c('tr', [_c('td', [_vm._v("append")]), _vm._v(" "), _c('td', [_vm._v("Boolean")]), _vm._v(" "), _c('td', [_c('code', {
+    pre: true
+  }, [_vm._v("false")])]), _vm._v(" "), _c('td')]), _vm._v(" "), _c('tr', [_c('td', [_vm._v("disabled")]), _vm._v(" "), _c('td', [_vm._v("Boolean")]), _vm._v(" "), _c('td', [_c('code', {
+    pre: true
+  }, [_vm._v("false")])]), _vm._v(" "), _c('td', [_vm._v("Prevent action of this link")])])])]), _vm._v(" "), _c('h3', [_vm._v("Slots")]), _vm._v(" "), _c('table', [_c('thead', [_c('tr', [_c('th', [_vm._v("Slot")]), _vm._v(" "), _c('th', [_vm._v("Description")])])]), _vm._v(" "), _c('tbody', [_c('tr', [_c('td', [_vm._v("(default)")]), _vm._v(" "), _c('td', [_vm._v("Hold the text and can contain HTML")])])])]), _vm._v(" "), _c('h3', [_vm._v("Events")]), _vm._v(" "), _c('h4', [_vm._v("$listeners")]), _vm._v(" "), _c('p', [_vm._v("Inherit all events from "), _c('code', {
+    pre: true
+  }, [_vm._v("<a>")]), _vm._v(" element.")]), _vm._v(" "), _c('h4', [_vm._v("@click")]), _vm._v(" "), _c('h4', [_vm._v("@navigate")])])
+}
+var staticRenderFns = []
+var esExports = { render: render, staticRenderFns: staticRenderFns }
+/* harmony default export */ __webpack_exports__["a"] = (esExports);
+
+/***/ })
+
+});
