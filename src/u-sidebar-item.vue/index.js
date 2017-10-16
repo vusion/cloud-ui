d4726cdee59cd53a48dc205aa056a77d6bf1f2c1
郝绪彤 <1255965516@qq.com>
2017-10-16 11:10:57 +0800

    :sparkles: Add refresh page make selected u-sidebar-menu open function

--- src/u-sidebar-item.vue/index.js
+++ src/u-sidebar-item.vue/index.js
@@ -3,4 +3,32 @@ import RouterItem from 'u-router-item.vue';
 export default {
     name: 'u-sidebar-item',
     mixins: [RouterItem],
+    computed: {
+        active() {
+            if (this.to === undefined)
+                return;
+
+            if (!this.$router)
+                return console.warn('[proto-ui] Cannot find vue router.');
+
+            const current = this.$route;
+            const location = this.$router.resolve(this.to).location;
+
+            if (this.exact) {
+                if (location.path === current.path) {
+                    if (this.$parent.$options.name === 'u-sidebar-menu')
+                        this.$parent.currentOpen = true;
+                    return true;
+                } else
+                    return false;
+            } else {
+                if (current.path.startsWith(location.path)) {
+                    if (this.$parent.$options.name === 'u-sidebar-menu')
+                        this.$parent.currentOpen = true;
+                    return true;
+                } else
+                    return false;
+            }
+        },
+    },
 };
