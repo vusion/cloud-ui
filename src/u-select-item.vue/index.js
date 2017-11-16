c6d35a3560c65d7c29833ceaad84cae2004ff9b2
haoxutong <1255965516@qq.com>
2017-11-16 23:03:34 +0800

    :sparkles: Add title in u-select-item

--- src/u-select-item.vue/index.js
+++ src/u-select-item.vue/index.js
@@ -4,6 +4,12 @@ export default {
     name: 'u-select-item',
     parentName: 'u-select',
     mixins: [ListViewItem],
+    props: {
+        field: String,
+        value: null,
+        disabled: { type: Boolean, default: false },
+        item: Object,
+    },
     created() {
         const parentVM = this.$parent.parentVM.$parent;
         parentVM.$emit('add-item-vm', this);
