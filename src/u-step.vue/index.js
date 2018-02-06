ec435d5dca370ab55fe6402ef0a5059499750800
Rainfore <rainforest92@126.com>
2018-02-06 15:46:15 +0800

    :ok_hand: Review code

--- src/u-step.vue/index.js
+++ src/u-step.vue/index.js
@@ -1,28 +1,3 @@
-import Emitter from 'proto-ui.vusion/src/u-emitter.vue';
-import RouterItem from 'proto-ui.vusion/src/u-router-item.vue';
+import Step from 'proto-ui.vusion/src/u-step.vue';
 
-export default {
-    name: 'u-step',
-    parentName: 'u-steps',
-    mixins: [Emitter, RouterItem],
-    props: {
-        title: String,
-    },
-    data() {
-        return {
-            parentVM: undefined,
-        };
-    },
-    computed: {
-        selected() {
-            if (this.parentVM)
-                return this.parentVM.router ? this.active : this.parentVM.selectedVM === this;
-        },
-    },
-    created() {
-        this.dispatch(this.$options.parentName, 'add-item-vm', this);
-    },
-    destroyed() {
-        this.dispatch(this.$options.parentName, 'remove-item-vm', this);
-    },
-};
+export default Step;
--- src/u-tooltip.vue/README.md
+++ src/u-tooltip.vue/README.md
@@ -3,13 +3,9 @@
 ## 示例
 ### 基本形式
 
-#### 声明式
 ``` html
 <u-tooltip size="normal">
     <u-button>hover</u-button>
-    <div slot="content">
-        <div>提示内容</div>
-    </div>
 </u-tooltip>
 ```
 
