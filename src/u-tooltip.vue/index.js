ce34c391eeffb2716be7f0f2c7508cff59a055c9
Rainfore <rainforest92@126.com>
2018-02-28 22:52:20 +0800

    :lipstick: Make position of <u-select-item> relative

--- src/u-tooltip.vue/index.js
+++ src/u-tooltip.vue/index.js
@@ -1,14 +1,9 @@
 export default {
     name: 'u-tooltip',
     props: {
-        content: {
-            type: String,
-            default: '提示内容',
-        },
-        placement: {
-            type: String,
-            default: 'bottom',
-        },
+        content: { type: String, default: '提示内容' },
+        placement: { type: String, default: 'bottom' },
+        reference: HTMLElement, // 为了方便生成指令
         contentWidth: {
             type: String,
             default: 'auto',
