497ad6a630287709c5c1f1f85aa50d7def3cb5af
Rainfore <rainforest92@126.com>
2018-03-01 10:28:50 +0800

    :fire: Use super <u-link> from proto-ui

--- src/u-link.vue/index.js
+++ src/u-link.vue/index.js
@@ -1,35 +1,3 @@
 import Link from 'proto-ui.vusion/src/u-link.vue';
 
-export default {
-    name: 'u-link',
-    mixins: [Link],
-    props: {
-        target: { type: String, default: '_self' },
-    },
-    computed: {
-        currentHref() {
-            if (this.href !== undefined)
-                return this.href;
-            else if (this.$router && this.to)
-                return this.$router.resolve(this.to, this.$route, this.append).href;
-            else
-                return undefined;
-        },
-    },
-    methods: {
-        onClick(e) {
-            if (this.disabled)
-                return e.preventDefault();
-
-            this.$emit('click', e);
-
-            if (this.target !== '_self')
-                return;
-
-            if (this.href === undefined) {
-                e.preventDefault();
-                this.navigate();
-            }
-        },
-    },
-};
+export default Link;
