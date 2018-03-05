29d48ee270735af40dddb2cd3bc6976b534bbe02
luoxl <luoxlstayrealdream@163.com>
2018-03-05 16:00:22 +0800

    :sparkle: <u-time-picker>

--- src/u-time-picker.vue/README.md
+++ src/u-time-picker.vue/README.md
@@ -7,34 +7,37 @@
 
 ### 基本
 ``` html
-<u-time-picker width="56" time="15:46:33"></u-time-picker>
+<u-time-picker time="15:46:33"></u-time-picker>
+```
+### 显示时分
+``` html
+<u-time-picker time="15:46:33" tag="110"></u-time-picker>
 ```
 
-### 禁用
+### 禁用和只读
 ``` html
-<u-time-picker width="56" time="15:46:33" disabled="disabled"></u-time-picker>
+<u-linear-layout>
+<u-time-picker time="15:46:33" disabled="disabled"></u-time-picker>
+<u-time-picker time="15:46:33" readonly="readonly"></u-time-picker>
+</u-linear-layout>
 ```
 
-### 只读
-``` html
-<u-time-picker width="56" time="15:46:33" readonly="readonly"></u-time-picker>
-```
 
 ### 日期范围
 ``` html
-<u-time-picker time="15:46:33" width="56" min-time="12:30:00" max-time="14:45:00"></u-time-picker>
+<u-time-picker time="15:46:33" min-time="12:30:00" max-time="14:45:00"></u-time-picker>
 ```
 
 ``` vue
 <template>
-<u-time-picker width="56" :time="time" @change="change($event.time)"></u-time-picker>
+<u-time-picker :time="time" @change="change($event)"></u-time-picker>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			time: '20:12:12',
+			time: '1:12:12',
 		};
 	},
     methods: {
@@ -45,3 +48,25 @@ export default {
 };
 </script>
 ```
+## API
+### Props/Attrs
+
+| Prop/Attr | Type | Default | Description |
+| --------- | ---- | ------- | ----------- |
+| time.sync, v-model | String | `'00:00:00'` | 时间 |
+| minTime | String | `'00:00:00'` | 最小时间 |
+| maxTime | String | `'23:59:59'` | 最大时间 |
+| readonly | Boolean | `false` | 是否只读 |
+| disabled | Boolean | `false` | 是否禁用 |
+| tag | String | `111` | 控制显示时分秒的显示，1为显示，0为不显示 |
+
+### Events
+
+#### @change
+
+值变化时触发（与原生事件不同）
+
+| Param | Type | Description |
+| ----- | ---- | ----------- |
+| $event.value | String | 改变后的值 |
+| $event.oldValue | String | 旧的值 |
--- src/u-time-picker.vue/index.js
+++ src/u-time-picker.vue/index.js
@@ -1,12 +1,27 @@
-import NumberInput from '../u-number-input.vue';
+// 默认时间及其校验
+const defaultRangeArr = [[0, 23], [0, 59], [0, 59]];
+const splitTag = ':';
+const timeValidator = (time) => {
+    const timeArr = time.split(splitTag);
+    if (timeArr.length !== 3)
+        throw new Error(time + ' format error');
 
-const HOUR_MIN = '00';
-const MINUTE_MIN = '00';
-const SECOND_MIN = '00';
-const HOUR_MAX = '23';
-const MINUTE_MAX = '59';
-const SECOND_MAX = '59';
-
+    const isNumber = timeArr.every((t, index) => !isNaN(t) && +t >= defaultRangeArr[index][0] && +t <= defaultRangeArr[index][1]);
+    if (!isNumber)
+        throw new Error(time + ' is not Number');
+    return true;
+};
+const formatTime = (time) => {
+    const res = [];
+    time.split(splitTag).forEach((t) => {
+        const numberT = +t;
+        if (numberT < 10)
+            res.push('0' + numberT);
+        else
+            res.push(numberT);
+    });
+    return res.join(splitTag);
+};
 /**
  * @class TimePicker
  * @extend Component
@@ -14,179 +29,124 @@ const SECOND_MAX = '59';
  * @param {string='00:00'}          options.data.time               <=> 当前的时间值
  * @param {string='00:00'}          options.data.minTime             => 最小时间
  * @param {string='23:59'}          options.data.maxTime             => 最大时间
- * @param {boolean=false}           options.data.autofocus           => 是否自动获得焦点
  * @param {boolean=false}           options.data.readonly            => 是否只读
  * @param {boolean=false}           options.data.disabled            => 是否禁用
  * @param {boolean=true}            options.data.visible             => 是否显示
  * @param {string=''}               options.data.class               => 补充class
  */
-const TimePicker = {
+export default {
     name: 'u-time-picker',
     props: {
         time: {
             type: String,
             default: '00:00:00',
+            validator: timeValidator,
         },
-        autofocus: [String, Boolean],
-        disabled: [String, Boolean],
-        readonly: [String, Boolean],
-        width: String,
+        disabled: { type: Boolean, default: false },
+        readonly: { type: Boolean, default: false },
+        tag: { type: String, default: '111', validator: (t) => t.split('').length === 3 && t.split('').every((item) => ['0', '1'].indexOf(item) !== -1) }, // 分别对应时分秒，时分表示为：110
         minTime: {
             type: String,
             default: '00:00:00',
+            validator: timeValidator,
         },
         maxTime: {
             type: String,
             default: '23:59:59',
+            validator: timeValidator,
         },
     },
     data() {
         return {
-            showTime: this.time,
-            hourmin: HOUR_MIN,
-            hourmax: HOUR_MAX,
-            minutemin: MINUTE_MIN,
-            minutemax: MINUTE_MAX,
-            secondmin: SECOND_MIN,
-            secondmax: SECOND_MAX,
+            hour: null,
+            minute: null,
+            second: null,
+            defaultRangeArr, // 时分秒默认的上下限
+            rangeArr: [], // 根据min和max记录时分秒的range
+            timeArr: [],
+            currentTime: formatTime(this.time), // 将时间转换为'xx:xx:xx'的格式，方便大小对比
+            currentMinTime: formatTime(this.minTime), // 当前时间
+            currentMaxTime: formatTime(this.maxTime), // 当前时间
         };
     },
-    computed: {
-        hour() {
-            const isOutOfRange = this.isOutOfRange(this.showTime);
-            if (isOutOfRange)
-                this.showTime = isOutOfRange;
-            return this.showTime.split(':')[0];
-        },
-        minute() {
-            const isOutOfRange = this.isOutOfRange(this.showTime);
-            if (isOutOfRange)
-                this.showTime = isOutOfRange;
-            return this.showTime.split(':')[1];
-        },
-        second() {
-            const isOutOfRange = this.isOutOfRange(this.showTime);
-            if (isOutOfRange)
-                this.showTime = isOutOfRange;
-            return this.showTime.split(':')[2];
-        },
-        sphourmin() {
-            return this.minTime.split(':')[0];
-        },
-        sphourmax() {
-            return this.maxTime.split(':')[0];
-        },
-        spminutemin() {
-            return this.minTime.split(':')[1];
-        },
-        spminutemax() {
-            return this.maxTime.split(':')[1];
-        },
-        spsecondmin() {
-            return this.minTime.split(':')[2];
-        },
-        spsecondmax() {
-            return this.maxTime.split(':')[2];
-        },
+    created() {
+        this.currentTime = this.getRangeTime(); // 根据时间Range设置初始时间
+        this.initRange(); // 根据时间Range初始化时分秒的上限
+        this.initTimeArr(); // 初始化time-picker的每项value，max，min等
     },
     watch: {
-        time(newValue) {
-            if (!newValue)
-                throw new TypeError('Invalid Time');
-
-            // 如果超出时间范围，则设置为范围边界的时间
-            const isOutOfRange = this.isOutOfRange(newValue);
-            if (isOutOfRange)
-                return this.showTime = isOutOfRange;
-            this.showTime = newValue;
+        time(value) {
+            this.currentTime = formatTime(value);
         },
-        showTime(newValue) {
-            if (!newValue)
-                throw new TypeError('Invalid Time');
-
-            // 如果超出时间范围，则设置为范围边界的时间
-            const isOutOfRange = this.isOutOfRange(newValue);
-            if (isOutOfRange)
-                return this.showTime = isOutOfRange;
-            if (this.showTime === this.minTime) {
-                this.hourmin = this.sphourmin;
-                this.minutemin = this.spminutemin;
-                this.secondmin = this.spsecondmin;
-            } else if (this.showTime === this.maxTime) {
-                this.hourmax = this.sphourmax;
-                this.minutemax = this.spminutemax;
-                this.secondmax = this.spsecondmax;
-            } else {
-                this.hourmin = HOUR_MIN;
-                this.minutemin = MINUTE_MIN;
-                this.secondmin = SECOND_MIN;
-                this.hourmax = HOUR_MAX;
-                this.minutemax = MINUTE_MAX;
-                this.secondmax = SECOND_MAX;
-            }
-
-            /**
-             * @event change 时间改变时触发
-             * @property {object} sender 事件发送对象
-             * @property {object} time 改变后的时间
-             */
-            this.$emit('change', {
-                sender: this,
-                time: newValue,
-            });
-        },
-        minTime(newValue, oldValue) {
-            this.hourmin = HOUR_MIN;
-            this.minutemin = MINUTE_MIN;
-            this.secondmin = SECOND_MIN;
-            this.hourmax = HOUR_MAX;
-            this.minutemax = MINUTE_MAX;
-            this.secondmax = SECOND_MAX;
-        },
-        maxTime(newValue, oldValue) {
-            this.hourmin = HOUR_MIN;
-            this.minutemin = MINUTE_MIN;
-            this.secondmin = SECOND_MIN;
-            this.hourmax = HOUR_MAX;
-            this.minutemax = MINUTE_MAX;
-            this.secondmax = SECOND_MAX;
+        currentTime(value, oldValue) {
+            this.$emit('change', { value, oldValue });
+            this.$emit('update:time', value);
         },
     },
     methods: {
+        initRange() { // 根据范围设置 时分秒 上下限
+            const minArr = this.minTime.split(splitTag);
+            const maxArr = this.maxTime.split(splitTag);
+            for (let i = 0; i < 3; ++i)
+                this.rangeArr.push([+minArr[i], +maxArr[i]]);
+        },
+        // 初始化timeArr
+        initTimeArr() {
+            const tagArr = this.tag.split('');
+            this.currentTime.split(splitTag).forEach((v, index) => {
+                const timeInfo = {
+                    min: this.rangeArr[index][0],
+                    max: this.rangeArr[index][1],
+                    value: +v,
+                    onChange: this.onChange,
+                    show: tagArr[index] === '1',
+                };
+                this.timeArr.push(timeInfo);
+            });
+            this.setRange(0); // 根据当前value设置时分秒的上下限
+        },
+        setRange(index) {
+            if (index >= 2) {
+                this.$forceUpdate();
+                return;
+            }
+            const curTimeObj = this.timeArr[index];
+            const curRangeObj = this.rangeArr[index];
+            const next = index + 1;
+            if (curTimeObj.value === curRangeObj[0])
+                this.timeArr[next].min = this.rangeArr[next][0];
+            else if (curTimeObj.value === curRangeObj[1])
+                this.timeArr[next].max = this.rangeArr[next][1];
+            else { // 重置上下限
+                this.timeArr[next].max = this.defaultRangeArr[next][1];
+                this.timeArr[next].min = this.defaultRangeArr[next][0];
+            }
+            this.setRange(next);
+        },
         /**
-         * @method isOutOfRange(time) 是否超出规定的时间范围
+         * @method getRangeTime(time) 得到range范围内的时间
          * @public
-         * @param {Time} time 待测的时间
-         * @return {boolean|Time} time 如果没有超出时间范围，则返回false；如果超出时间范围，则返回范围边界的时间
+         * @return {String} time 如果没有超出时间范围，则返回时间；如果超出时间范围，则返回范围边界的时间
          */
-        isOutOfRange(time) {
-            const minTime = this.minTime;
-            const maxTime = this.maxTime;
+        getRangeTime(time = this.currentTime) {
+            const tempMax = this.currentMinTime > time ? this.currentMinTime : time;
+            return tempMax > this.currentMaxTime ? this.currentMaxTime : tempMax;
+        },
+        onChange(e, index) {
+            const cTimeArr = this.currentTime.split(splitTag);
+            cTimeArr[index] = e.formatValue;
+            this.currentTime = cTimeArr.join(splitTag);
+            this.timeArr[index].value = e.value;
+            this.setRange(index);
+        },
+        formatFun(len) {
+            return (val) => {
+                if (('' + val).length >= len)
+                    return val;
 
-            // minTime && time < minTime && minTime，先判断是否为空，再判断是否超出范围，如果超出则返回范围边界的时间
-            return (minTime && time < minTime && minTime) || (maxTime && time > maxTime && maxTime);
-        },
-        changeHour(hour) {
-            hour = '' + hour;
-            this.showTime = (hour.length > 1 ? hour : '0' + hour) + ':' + this.minute + ':' + this.second;
-        },
-        changeMinute(minute) {
-            minute = '' + minute;
-            this.showTime = this.hour + ':' + (minute.length > 1 ? minute : '0' + minute) + ':' + this.second;
-        },
-        changeSecond(second) {
-            second = '' + second;
-            this.showTime = this.hour + ':' + this.minute + ':' + (second.length > 1 ? second : '0' + second);
+                return (Array(len + 1).join('0') + val).slice(-len);
+            };
         },
     },
 };
 
-const TimeRangeError = function (minTime, maxTime) {
-    this.name = 'TimeRangeError';
-    this.message = 'Wrong Time Range where `minTime` is ' + minTime + ' and `maxTime` is ' + maxTime + '!';
-};
-
-TimeRangeError.prototype = Object.create(Error.prototype);
-TimePicker.TimeRangeError = TimeRangeError.prototype.constructor = TimeRangeError;
-
-export default TimePicker;
