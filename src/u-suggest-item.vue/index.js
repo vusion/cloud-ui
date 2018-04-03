6150115e467bb5aecf19a926309eae6d2c1ea086
Rainfore <rainforest92@126.com>
2018-04-03 15:07:45 +0800

    :bug: Remove duplicated <u-select-item>, <u-suggest-item>

--- src/u-suggest-item.vue/index.js
+++ src/u-suggest-item.vue/index.js
@@ -4,19 +4,4 @@ export default {
     name: 'u-suggest-item',
     parentName: 'u-suggest',
     mixins: [ListViewItem],
-    props: {
-        field: String,
-    },
-    created() {
-        let popperChildVM = this.$parent;
-        while (popperChildVM.$options.name !== 'u-popper-child')
-            popperChildVM = popperChildVM.$parent;
-        if (popperChildVM && popperChildVM.parentVM) {
-            this.parentVM = popperChildVM.parentVM.$parent;
-            this.parentVM.$emit('add-item-vm', this);
-        }
-    },
-    destroyed() {
-        this.parentVM && this.parentVM.$emit('remove-item-vm', this);
-    },
 };
