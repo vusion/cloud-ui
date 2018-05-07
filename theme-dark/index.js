5039f735fa94200bafa39ab45b00c039b3403bdc
shaoyuee <13588317944@163.com>
2018-05-07 17:47:57 +0800

    :lipstick: Add <u-sidebar-group>、<u-sidebar> in theme-dark

--- theme-dark/index.js
+++ theme-dark/index.js
@@ -1,14 +1,18 @@
 import Button from './u-button.vue';
 import Link from './u-link.vue';
+import Sidebar from './u-sidebar.vue';
 import SidebarDivider from './u-sidebar-divider.vue';
+import SidebarGroup from './u-sidebar-group.vue';
 import SidebarItem from './u-sidebar-item.vue';
 import SubnavItem from './u-subnav-item.vue';
 
 export {
     Button,
     Link,
-    SidebarItem,
+    Sidebar,
     SidebarDivider,
+    SidebarGroup,
+    SidebarItem,
     SubnavItem,
 };
 export {
@@ -80,8 +84,6 @@ export {
     SelectItem,
     SuggestItem,
     Suggest,
-    Sidebar,
-    SidebarGroup,
     Slider,
     StatusIcon,
     Steps,
