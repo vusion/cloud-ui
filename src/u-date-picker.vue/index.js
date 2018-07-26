0b44020193da41d7b4558543bb6a0496274e4ff2
luoxlgh <154206273@qq.com>
2018-07-26 10:34:36 +0800

    :lipstick: modify <u-picker>

--- src/u-date-picker.vue/index.js
+++ src/u-date-picker.vue/index.js
@@ -1,6 +1,11 @@
+import isEqual from 'date-fns/is_equal';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+
 import Picker from '../u-picker.vue';
 import pickerInput from '../u-picker.vue/input/u-picker-input.vue';
 import pickPanel from '../u-picker.vue/panel/u-panel-control.vue';
+import { inDateRange, _isDate, validateDateRange } from '../u-picker.vue/panel/date';
 
 export default {
     name: 'u-date-picker',
@@ -12,5 +17,103 @@ export default {
     props: {
         placeholder: { type: String, default: '请选择日期' },
         type: { type: String, default: 'date', validator: (value) => ['calendar', 'year', 'month', 'date'].includes(value) },
+    },
+    watch: {
+        value(value, oldValue) {
+            if (!this.inDateRange(value, this.currentDateRange))
+                throw new RangeError('initital date is out of dateRange');
+            if (isEqual(value, oldValue))
+                return;
+
+            this.inputValue = format(value, this.currentFormatter);
+            this.currentValue = value;
+        },
+        inputValue(value, oldValue) { // input框值emit更新
+            if (value === this.changedInputValue)
+                return;
+            if (value !== undefined) {
+                if (this.inDateRange(value, this.currentDateRange)) {
+                    this.inputValue = format(value, this.currentFormatter);
+                } else {
+                    this.inputValue = oldValue;
+                }
+                this.changedInputValue = this.inputValue; // 防止循环赋值
+            }
+            this.currentValue = this.inputValue;
+            this.$refs.input.forceUpdateValue();
+        },
+        currentValue(value, oldValue) {
+            this.$emit('update:value', this.formatValue(value));
+            this.inputValue = value ? format(value, this.currentFormatter) : value;
+            if (isEqual(value, oldValue))
+                return;
+            this.$emit('change', {
+                value: this.setDateTime(parse(value)),
+                oldValue: this.setDateTime(parse(oldValue)),
+                formattedValue: this.inputValue,
+            });
+        },
+        type(value) {
+            this.initFormatter();
+            this.initPanelControl();
+        },
+    },
+    methods: {
+        inDateRange,
+        validateRange: validateDateRange,
+        // date面板和time面板的跳转控制
+        initFormatter() {
+            let res;
+            switch (this.type) {
+                case 'year':
+                    res = /^(Y)+$/.exec(this.currentFormatter);
+                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY';
+                    break;
+                case 'month':
+                    res = /^(Y+[^a-zA-Z]M+)$/.exec(this.currentFormatter);
+                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY-MM';
+                    break;
+                case 'date':
+                    res = /^(Y+[^a-zA-Z]M+[^a-zA-Z]D+)$/.exec(this.currentFormatter);
+                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY-MM-DD';
+                    break;
+            }
+            this.inputValue = format(this.currentValue, this.currentFormatter);
+        },
+        initPanelControl() {
+            this.blockPanel = {};
+            if (!/(Y)+/.exec(this.currentFormatter))
+                this.blockPanel.year = true;
+            else
+                this.panelView = 'year';
+            if (!/(M)+/.exec(this.currentFormatter))
+                this.blockPanel.month = true;
+            else
+                this.panelView = 'month';
+            if (!/(D)+/.exec(this.currentFormatter))
+                this.blockPanel.day = true;
+            else
+                this.panelView = 'day'; // 面板显示优先级： day > month > year
+        },
+        onPanelDateSelect(event) {
+            if (this.disabled || this.readonly)
+                return;
+            let cancel = false;
+            this.$emit('before-select', {
+                preventDefault: () => cancel = true,
+            });
+
+            if (cancel)
+                return;
+
+            this.currentValue = event.value;
+            const value = this.setDateTime(event.value);
+            this.$emit('select', {
+                value,
+                oldValue: this.setDateTime(event.oldValue),
+                formattedValue: format(value, this.currentFormatter),
+            });
+            this.closePanel();
+        },
     },
 };
--- src/u-picker.vue/index.js
+++ src/u-picker.vue/index.js
@@ -20,14 +20,11 @@
  * panel抛出select, beforeSelect事件。 date都为Date格式，交由picker格式化
  * picker抛出toggle。before-select, select（不论有效值是否改变，用户点击即触发）. change(两个validate的值变化)
  */
-import isEqual from 'date-fns/is_equal';
 import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import { dateValidadtor, validateDateRange, inDateRange, setDateTime, _isDate } from './panel/date';
+import { dateValidadtor, setDateTime } from './panel/date';
 
 export default {
     name: 'u-picker',
-    // inheritAttrs: false,
     props: {
         disabled: { type: Boolean, default: false },
         readonly: { type: Boolean, default: false },
@@ -62,7 +59,6 @@ export default {
     data() {
         return {
             showPanel: false,
-            hasInit: false,
             currentFormatter: this.formatter,
             currentDateRange: [],
             blockPanel: {}, // 不能跳转到的页面
@@ -73,136 +69,36 @@ export default {
         };
     },
     computed: {
-        init() { // 控制panel mount时机 ??? 干啥的
-            if (this.hasInit)
-                return true;
-            if (this.showPanel) {
-                this.hasInit = true;
-                return true;
-            }
-            return false;
-        },
         allProps() {
-            return Object.assign({}, this.$props, this.$attrs);// 也是响应式的
-        },
-    },
-    watch: {
-        value(value, oldValue) {
-            if (!inDateRange(value, this.currentDateRange))
-                throw new RangeError('initital date is out of dateRange');
-            if (isEqual(value, oldValue))
-                return;
-
-            this.inputValue = format(value, this.currentFormatter);
-            this.currentValue = value;
-        },
-        inputValue(value, oldValue) { // input框值emit更新
-            if (value === this.changedInputValue)
-                return;
-            if (value !== undefined) {
-                if (inDateRange(value, this.currentDateRange)) {
-                    this.inputValue = format(value, this.currentFormatter);
-                } else {
-                    this.inputValue = oldValue;
-                }
-                this.changedInputValue = this.inputValue; // 防止循环赋值
-            }
-            this.currentValue = this.inputValue;
-            this.$refs.input.forceUpdateValue();
-        },
-        currentValue(value, oldValue) {
-            this.$emit('update:value', this.formatValue(value));
-            this.inputValue = value ? format(value, this.currentFormatter) : value;
-            if (isEqual(value, oldValue))
-                return;
-            this.$emit('change', {
-                value: this.setDateTime(parse(value)),
-                oldValue: this.setDateTime(parse(oldValue)),
-                formattedValue: this.inputValue,
-            });
-        },
-        type(value) {
-            this.initFormatter();
-            this.initPanelControl();
+            return Object.assign({}, this.$props, this.$data);// 也是响应式的
         },
     },
     created() {
-        this.validateData();
+        this.validateData && this.validateData(); // 根据minDate, maxDate和dateRange生成currentDateRange，并判断初始值是否在range内
 
-        this.initFormatter();
-        this.initPanelControl();
+        this.initFormatter && this.initFormatter(); // 根据type生成currentFormatter
+        this.initPanelControl && this.initPanelControl(); // 根据currentFormatter判断可以跳入的页面
     },
     methods: {
+        inDateRange() {
+            return true;
+        },
         onToggle($event) {
             this.$emit('toggle', $event);
             this.showPanel = $event.open;
         },
-        onPanelDateSelect(event) {
-            if (this.disabled || this.readonly)
-                return;
-            let cancel = false;
-            this.$emit('before-select', {
-                preventDefault: () => cancel = true,
-            });
-
-            if (cancel)
-                return;
-
-            this.currentValue = event.value;
-            const value = this.setDateTime(event.value);
-            this.$emit('select', {
-                value,
-                oldValue: this.setDateTime(event.oldValue),
-                formattedValue: format(value, this.currentFormatter),
-            });
-            this.closePanel();
-        },
         validateData() {
             const tempDateRange = (this.dateRange || []).concat([]);
-            validateDateRange(tempDateRange, this.minDate, this.maxDate);
+            this.validateRange && this.validateRange(tempDateRange, this.minDate, this.maxDate);
             this.currentDateRange = tempDateRange;
-            if (!inDateRange(this.currentValue, this.currentDateRange))
+            if (!this.inDateRange(this.currentValue, this.currentDateRange))
                 throw new RangeError('initital date is out of dateRange');
-        },
-        // date面板和time面板的跳转控制
-        initFormatter() {
-            let res;
-            switch (this.type) {
-                case 'year':
-                    res = /^(Y)+$/.exec(this.currentFormatter);
-                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY';
-                    break;
-                case 'month':
-                    res = /^(Y+[^a-zA-Z]M+)$/.exec(this.currentFormatter);
-                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY-MM';
-                    break;
-                case 'date':
-                    res = /^(Y+[^a-zA-Z]M+[^a-zA-Z]D+)$/.exec(this.currentFormatter);
-                    this.currentFormatter = res && _isDate(parse(new Date(), res[0])) ? res[0] : 'YYYY-MM-DD';
-                    break;
-            }
-            this.inputValue = format(this.currentValue, this.currentFormatter);
-        },
-        initPanelControl() {
-            this.blockPanel = {};
-            if (!/(Y)+/.exec(this.currentFormatter))
-                this.blockPanel.year = true;
-            else
-                this.panelView = 'year';
-            if (!/(M)+/.exec(this.currentFormatter))
-                this.blockPanel.month = true;
-            else
-                this.panelView = 'month';
-            if (!/(D)+/.exec(this.currentFormatter))
-                this.blockPanel.day = true;
-            else
-                this.panelView = 'day'; // 面板显示优先级： day > month > year
         },
         /**
          * 返回与this.value同样类型的数据
          */
         formatValue(date) {
-            const type = typeof this.value;
+            const type = typeof (this.value || this.startValue);
             switch (type) {
                 case 'number':
                     return +date;
