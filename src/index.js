75796a01067ce46ff27ca826545f52aea876b621
Rainfore <rainforest92@126.com>
2019-07-09 20:50:16 +0800

    :fire: Remove base.css import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import './base/index.css';
 export * from './components';
 
 import * as directives from './directives';
 import * as utils from './utils';
-
 export { directives, utils };
 
 export { install } from 'vusion-utils';
