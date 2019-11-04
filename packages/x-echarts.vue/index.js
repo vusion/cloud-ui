577b6e825e33a565054f2ddb48afcedee2818404
Rainfore <rainforest92@126.com>
2019-11-04 10:03:13 +0800

    :bug: Fix bug of x-echarts.vue

--- packages/x-echarts.vue/index.js
+++ packages/x-echarts.vue/index.js
@@ -2,6 +2,7 @@ import './theme';
 import VueEcharts from 'vue-echarts';
 
 export const XEcharts = {
+    name: 'x-echarts',
     extends: VueEcharts,
     props: {
         theme: { type: [String, Object], default: 'cloud-ui' },
