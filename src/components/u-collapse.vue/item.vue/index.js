05aa8ab26a27cebe7988433c8395269ea213faa5
daysai <wddpwzz36@gmail.com>
2020-03-22 17:15:39 +0800

    :lipstick: UCollapse 的样式修正

--- src/components/u-collapse.vue/item.vue/index.js
+++ src/components/u-collapse.vue/item.vue/index.js
@@ -1,4 +1,5 @@
 import { MChild } from '../../m-parent.vue';
+import { addClass, removeClass } from '../../../utils/dom';
 
 export const UCollapseItem = {
     name: 'u-collapse-item',
@@ -62,6 +63,66 @@ export const UCollapseItem = {
             if (expanded === undefined)
                 expanded = !this.currentExpanded;
             expanded ? this.expand() : this.collapse();
+        },
+
+        // collapse-transition javaScript hooks
+        beforeEnter(el) {
+            addClass(el, 'collapse-transition');
+            if (!el.dataset)
+                el.dataset = {};
+
+            el.dataset.oldPaddingTop = el.style.paddingTop;
+            el.dataset.oldPaddingBottom = el.style.paddingBottom;
+
+            el.style.height = '0';
+            el.style.paddingTop = 0;
+            el.style.paddingBottom = 0;
+        },
+        enter(el) {
+            el.dataset.oldOverflow = el.style.overflow;
+            if (el.scrollHeight !== 0) {
+                el.style.height = el.scrollHeight + 'px';
+                el.style.paddingTop = el.dataset.oldPaddingTop;
+                el.style.paddingBottom = el.dataset.oldPaddingBottom;
+            } else {
+                el.style.height = '';
+                el.style.paddingTop = el.dataset.oldPaddingTop;
+                el.style.paddingBottom = el.dataset.oldPaddingBottom;
+            }
+
+            el.style.overflow = 'hidden';
+        },
+        afterEnter(el) {
+            // for safari: remove class then reset height is necessary
+            removeClass(el, 'collapse-transition');
+            el.style.height = '';
+            el.style.overflow = el.dataset.oldOverflow;
+        },
+        beforeLeave(el) {
+            if (!el.dataset)
+                el.dataset = {};
+            el.dataset.oldPaddingTop = el.style.paddingTop;
+            el.dataset.oldPaddingBottom = el.style.paddingBottom;
+            el.dataset.oldOverflow = el.style.overflow;
+
+            el.style.height = el.scrollHeight + 'px';
+            el.style.overflow = 'hidden';
+        },
+        leave(el) {
+            if (el.scrollHeight !== 0) {
+                // for safari: add class after set height, or it will jump to zero height suddenly, weired
+                addClass(el, 'collapse-transition');
+                el.style.height = 0;
+                el.style.paddingTop = 0;
+                el.style.paddingBottom = 0;
+            }
+        },
+        afterLeave(el) {
+            removeClass(el, 'collapse-transition');
+            el.style.height = '';
+            el.style.overflow = el.dataset.oldOverflow;
+            el.style.paddingTop = el.dataset.oldPaddingTop;
+            el.style.paddingBottom = el.dataset.oldPaddingBottom;
         },
     },
 };
