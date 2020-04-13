04665239d9f574c69d1d608d147ca53b15e94081
daysai <wddpwzz36@gmail.com>
2020-04-13 19:24:15 +0800

    :pencil: add label api

--- src/components/u-timeline.vue/index.js
+++ src/components/u-timeline.vue/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable multiline-ternary */
 export const UTimeline = {
     name: 'u-timeline',
     props: {
@@ -16,15 +17,26 @@ export const UTimeline = {
         };
     },
     data() {
-        const hasLabel = this.$slots && this.$slots.default ? this.$slots.default.some((Vnode) => {
-            if (Vnode && Vnode.componentOptions) {
-                const componentOptions = Vnode.componentOptions;
-                const hasLabelProps = componentOptions.propsData ? !!componentOptions.propsData.label : false;
-                const hasLabelChild = componentOptions.children ? componentOptions.children.some((Vn) => (Vn && Vn.data ? Vn.data.slot === 'label' : false)) : false;
-                return hasLabelProps || hasLabelChild;
-            }
-            return false;
-        }) : false;
+        const hasLabel
+            = this.$slots && this.$slots.default
+                ? this.$slots.default.some((Vnode) => {
+                    if (Vnode && Vnode.componentOptions) {
+                        const componentOptions = Vnode.componentOptions;
+                        const hasLabelProps = componentOptions.propsData
+                            ? !!componentOptions.propsData.label
+                            : false;
+                        const hasLabelChild = componentOptions.children
+                            ? componentOptions.children.some((Vn) =>
+                                (Vn && Vn.data
+                                    ? Vn.data.slot === 'label'
+                                    : false)
+                            )
+                            : false;
+                        return hasLabelProps || hasLabelChild;
+                    }
+                    return false;
+                })
+                : false;
         return {
             hasLabel,
         };
