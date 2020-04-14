7ad4f89ff720952d6db916ef04d16b719af66f9e
daysai <wddpwzz36@gmail.com>
2020-04-14 11:43:33 +0800

    :alien: add mode contains alternate and label

--- src/components/u-timeline.vue/index.js
+++ src/components/u-timeline.vue/index.js
@@ -2,43 +2,21 @@
 export const UTimeline = {
     name: 'u-timeline',
     props: {
-        alternate: {
-            type: Boolean,
-            default: false,
-        },
         pending: {
             type: Boolean,
             default: false,
+        },
+        mode: {
+            type: String,
+            default: 'default',
+            validator(value) {
+                return ['default', 'alternate', 'label'].indexOf(value) !== -1;
+            },
         },
     },
     provide() {
         return {
             timeline: this,
-        };
-    },
-    data() {
-        const hasLabel
-            = this.$slots && this.$slots.default
-                ? this.$slots.default.some((Vnode) => {
-                    if (Vnode && Vnode.componentOptions) {
-                        const componentOptions = Vnode.componentOptions;
-                        const hasLabelProps = componentOptions.propsData
-                            ? !!componentOptions.propsData.label
-                            : false;
-                        const hasLabelChild = componentOptions.children
-                            ? componentOptions.children.some((Vn) =>
-                                (Vn && Vn.data
-                                    ? Vn.data.slot === 'label'
-                                    : false)
-                            )
-                            : false;
-                        return hasLabelProps || hasLabelChild;
-                    }
-                    return false;
-                })
-                : false;
-        return {
-            hasLabel,
         };
     },
 };
