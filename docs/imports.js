5ab82a91c89260537d8163bf1bbf3554b541446f
Forrest <rainforest92@126.com>
2020-05-22 00:42:55 +0800

    :sparkles: 配了主题色

--- docs/imports.js
+++ docs/imports.js
@@ -1,5 +1,8 @@
-// import Vue from 'vue';
+import Vue from 'vue';
 // import VueI18n from 'vue-i18n';
 // Vue.use(VueI18n);
+
+import variables from 'themeCSS?variables';
+Vue.prototype.$theme = variables;
 
 import './message';
