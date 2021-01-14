90b12149635395eebec2d8e1bf63bbaa7d03edd9
Forrest <rainforest92@126.com>
2021-01-14 23:02:30 +0800

    :bug: Fix bug of select filterable

--- src/components/u-select.vue/index.vue
+++ src/components/u-select.vue/index.vue
@@ -461,6 +461,7 @@ export default {
             this.close();
         },
         focus() {
+            this.preventBlur = true;
             if (this.filterable)
                 this.$refs.input.focus();
         },
