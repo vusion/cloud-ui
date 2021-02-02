7c7947f6e94ebaab58875bc876fc51d5f7d0f1c0
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2021-02-02 16:51:10 +0800

    feat: support find key by dataPath

--- scripts/lcap/validator.js
+++ scripts/lcap/validator.js
@@ -5,14 +5,36 @@ const path = require('path');
 const fs = require('fs-extra');
 const ajv = new Ajv();
 const validate = ajv.compile(schema);
+const lodash = require('lodash');
 const components = require('./config');
+const args = require('yargs').argv;
+
 const root = path.join(__dirname, '../../src/components');
 components.every((component) => {
     if (component.show) {
         const targetFile = path.join(root, component.name + '.vue', 'api.yaml');
-        const valid = validate(yaml.load(fs.readFileSync(targetFile).toString()));
+        const context = yaml.load(fs.readFileSync(targetFile).toString());
+        const valid = validate(context);
         if (!valid) {
             console.log(targetFile, '\n', validate.errors);
+             /**
+             * yarn node scripts/lcap/validator.js --HELP=true
+             * 1. 开启辅助定位 key 的功能，自动找出必填的 key
+             */
+            if (args.HELP === 'true') {
+                validate.errors.forEach(errorObject => {
+                    /* 辅助寻找必填的缺失的 key */
+                    if (errorObject.keyword === 'required') {
+                        const currentPath = errorObject.dataPath.split('/');
+                        const keyTitle = errorObject.params.missingProperty;
+                        // 去掉第一个 ''
+                        currentPath.shift();
+                        lodash.set(context, `${currentPath.join('.')}.${keyTitle}`, '');
+                        fs.writeFileSync(targetFile, yaml.safeDump(context));
+                    }
+                });
+            }
+          
             return false;
         }
     }
