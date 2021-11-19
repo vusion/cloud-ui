850fddc4e2df62dc6e38b06f65a0ac75a64e9f06
Forrest <rainforest92@126.com>
2021-11-19 11:30:55 +0800

    调整目录结构

--- docs/views/components.vue
+++ docs/views/components.vue
@@ -2,25 +2,30 @@
 <div :class="$style.root">
     <div :class="$style.side" v-if="!message.singleton">
         <u-sidebar :class="$style.sidebar" size="small">
-            <u-checkbox :class="$style.checkbox" v-model="showDeprecated">显示废弃组件</u-checkbox>
-            <u-checkbox :class="$style.checkbox" v-model="showInfrequent">显示低频组件</u-checkbox>
+            <u-checkbox :class="$style.checkbox" v-model="showAdvanced">显示高级组件</u-checkbox>
+            <!-- <u-checkbox :class="$style.checkbox" v-model="showDeprecated">显示废弃组件</u-checkbox>
+            <u-checkbox :class="$style.checkbox" v-model="showInfrequent">显示低频组件</u-checkbox> -->
             <template v-for="group in groups">
                 <template v-if="!group.name">
-                    <u-sidebar-item v-for="material in group.children" :key="material.name" v-show="(!material.deprecated || showDeprecated) && (!material.infrequent || showInfrequent)"
-                                    :href="material.href" :to="material.to ? material.to : `/${type}/` + material.name" :target="material.target">
+                    <u-sidebar-item v-for="material in group.children" :key="material.name"
+                        v-show="(!material.advanced || showAdvanced) && (!material.deprecated || showDeprecated) && (!material.infrequent || showInfrequent)"
+                        :href="material.href" :to="material.to ? material.to : `/${type}/` + material.name" :target="material.target">
                         {{ camelName ? material.CamelName : material.name }}
-                        <u-label size="small" v-if="material.deprecated" color="warning">废弃</u-label>
-                        <small v-else :class="$style.alias">{{ material.alias }}</small>
-                        <u-label size="small" v-if="material.newest" color="primary">NEW</u-label>
-                        <u-label size="small" v-if="material.infrequent">低频</u-label>
+                        <small :class="$style.alias">{{ material.alias }}</small>
+                        &nbsp;<u-label size="small" v-if="material.advanced">高级</u-label>
+                        &nbsp;<u-label size="small" v-if="material.deprecated" color="warning">废弃</u-label>
+                        &nbsp;<u-label size="small" v-if="material.newest" color="primary">NEW</u-label>
+                        &nbsp;<u-label size="small" v-if="material.infrequent">低频</u-label>
                     </u-sidebar-item>
                 </template>
                 <u-sidebar-group v-else :key="group.name" :title="group.name">
-                    <u-sidebar-item v-for="material in group.children" :key="material.name" v-show="(!material.deprecated || showDeprecated) && (!material.infrequent || showInfrequent)"
-                                    :href="material.href" :to="material.to ? material.to : `/${type}/` + material.name" :target="material.target">
+                    <u-sidebar-item v-for="material in group.children" :key="material.name"
+                        v-show="(!material.advanced || showAdvanced) && (!material.deprecated || showDeprecated) && (!material.infrequent || showInfrequent)"
+                        :href="material.href" :to="material.to ? material.to : `/${type}/` + material.name" :target="material.target">
                         {{ camelName ? material.CamelName : material.name }}
-                        <u-label size="small" v-if="material.deprecated" color="warning">废弃</u-label>
-                        <small v-else :class="$style.alias">{{ material.alias }}</small>
+                        <small :class="$style.alias">{{ material.alias }}</small>
+                        &nbsp;<u-label size="small" v-if="material.advanced">高级</u-label>
+                        &nbsp;<u-label size="small" v-if="material.deprecated" color="warning">废弃</u-label>
                         &nbsp;<u-label size="small" v-if="material.newest" color="primary">NEW</u-label>
                         &nbsp;<u-label size="small" v-if="material.infrequent">低频</u-label>
                     </u-sidebar-item>
@@ -49,6 +54,7 @@ export default {
         return {
             message,
             groups: this.$docs.componentsGroups,
+            showAdvanced: true,
             showDeprecated: false,
             showInfrequent: false,
         };
@@ -59,7 +65,7 @@ export default {
 <style module>
 .side {
     position: fixed;
-    width: 210px;
+    width: var(--sidebar-width);
     top: var(--navbar-height);
     bottom: 0;
     overflow: hidden;
@@ -74,15 +80,9 @@ export default {
 }
 
 .checkbox[class] {
-    color: #8594aa;
     margin-left: 20px;
     margin-bottom: 10px;
     display: inline-block;
-}
-
-.checkbox[class] > span {
-    background: none;
-    border-color: #8594aa;
 }
 
 .alias {
@@ -90,7 +90,7 @@ export default {
 }
 
 .main {
-    margin-left: 240px;
+    margin-left: var(--sidebar-width);
     margin-right: 240px;
 }
 
