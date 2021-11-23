801c38dee698d0492fcb8dcb966231a4c9f58842
Forrest <rainforest92@126.com>
2021-11-23 10:20:45 +0800

    改变样式顺序 genThemeConfig

--- scripts/genThemeConfig/index.ts
+++ scripts/genThemeConfig/index.ts
@@ -13,7 +13,7 @@ export interface ThemeProperty {
 
 export interface ThemeComponent {
     name: string;
-    title: string;
+    title?: string;
     properties: {
         [name: string]: ThemeProperty,
     };    
@@ -23,7 +23,41 @@ const themeComponents: Array<ThemeComponent> = [];
 
 const cssContent = fs.readFileSync(path.join(__dirname, '../../src/styles/theme.css'), 'utf-8');
 const root = postcss.parse(cssContent);
-const _root = root.nodes.find((node) => node.type === 'rule' && node.selector === ':root');
+const _root = root.nodes.find((node) => node.type === 'rule' && node.selector === ':root') as postcss.Rule;
 
+let lastComponent: ThemeComponent;
+let lastProp: string;
+_root.nodes.forEach((node) => {
+    if (node.type === 'comment' && (node.raws.before && node.raws.before.includes('\n')) && node.text.includes('@component ')) {
+        const cap = /@component\s+([\w-]+)(\s+@hidden)?/.exec(node.text.trim());
+        const name = cap[1].trim();
+        const hidden = !!cap[2];
 
-console.log(root.nodes.length);
+        if ((!lastComponent || lastComponent.name !== name)) {
+            if (hidden) {
+                return lastComponent = undefined;
+            }
+
+            lastComponent && themeComponents.push(lastComponent);
+            lastComponent = {
+                name,
+                properties: {}
+            }
+        }
+    } else if (node.type === 'comment' && (!node.raws.before || !node.raws.before.includes('\n')) && node.text.trim() === '@hidden') {
+        delete lastComponent.properties[lastProp];
+    } else if (node.type === 'decl') {
+        if (!lastComponent)
+            return;
+        lastComponent.properties[node.prop] = {
+            type: 'input',
+            default: node.value,
+        };
+        lastProp = node.prop;
+    }
+});
+lastComponent && themeComponents.push(lastComponent);
+
+const result = JSON.stringify(themeComponents, null, 4)
+    .replace(/\{\n\s+"type": "(.+?)",\s+"default": "(.+?)"\s+}/g, `{ "type": "$1", "default": "$2" }`);
+fs.writeFileSync(path.join(__dirname, './result.json'), result, 'utf8');
