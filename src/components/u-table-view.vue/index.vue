327bfe486f83571552220f16287dc497cde8594e
violarong <zxrong@gmail.com>
2021-11-25 15:32:14 +0800

    :bug: fix u-table-view pageable false报错

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -419,7 +419,7 @@ export default {
         });
         this.debouncedLoad = debounce(this.load, 300);
         this.currentDataSource = this.normalizeDataSource(this.dataSource || this.data);
-        if (this.pageNumber) {
+        if (this.pageNumber && this.pageable) {
             this.page(this.pageNumber);
         }
         this.initialLoad && this.load();
