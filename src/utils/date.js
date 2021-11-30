8adf07d14cc137a197943c113168bd4b1901172d
violarong <zxrong@gmail.com>
2021-11-30 16:26:28 +0800

    :bug: safari transformDate 格式处理

--- src/utils/date.js
+++ src/utils/date.js
@@ -22,7 +22,7 @@ export const format = function format(value, type) {
             }
             return fix(date.getDate());
         },
-        QQ(date) { return `Q${Math.ceil((date.getMonth() + 1)/3)}`},
+        QQ(date) { return `Q${Math.ceil((date.getMonth() + 1) / 3)}`; },
         DD(date) { return fix(date.getDate()); },
         HH(date) { return fix(date.getHours()); },
         mm(date) { return fix(date.getMinutes()); },
@@ -47,66 +47,64 @@ export const transformDate = function transformDate(date) {
          * 不能做 - 的替换，会导致转化失效
          */
         if (date.includes('Q')) {
-            return new Date(date.replace(/Q1/,'1').replace(/Q2/,'4').replace(/Q3/,'7').replace(/Q4/,'10'));
+            return new Date(date.replace(/Q1/, '1').replace(/Q2/, '4').replace(/Q3/, '7').replace(/Q4/, '10'));
         }
+        date = date.replace(/-/g, '/');
         return new Date(date);
-    }
-    else if (typeof date === 'number')
+    } else if (typeof date === 'number')
         return new Date(date);
     else if (typeof date === 'object')
         return date;
 };
 
-
 /** 自动去掉最大最小时间的尾数 */
 export function ChangeDate(currentDate, picker, flag) {
-    const cloneDate = cloneDeep(currentDate)
+    const cloneDate = cloneDeep(currentDate);
     const date = new Date(cloneDate);
-   if (picker === 'month') {
-       date.setDate(1);
-       date.setHours(0, 0, 0, 0);
-       return date;
-   }
+    if (picker === 'month') {
+        date.setDate(1);
+        date.setHours(0, 0, 0, 0);
+        return date;
+    }
 
-   if (picker === 'quarter') {
-       // 更新为当前范围的最小值
-       const month = date.getMonth();
-       let currentMonth;
-       /** 2, 7, 10 作为季度的开始第一天 */
-       if (flag === 'min') {
-            if ( 0 <= month && month < 3) {
+    if (picker === 'quarter') {
+        // 更新为当前范围的最小值
+        const month = date.getMonth();
+        let currentMonth;
+        /** 2, 7, 10 作为季度的开始第一天 */
+        if (flag === 'min') {
+            if (0 <= month && month < 3) {
                 currentMonth = 0;
-            } else if ( month < 6) {
+            } else if (month < 6) {
                 currentMonth = 3;
-            } else if ( month < 9) {
+            } else if (month < 9) {
                 currentMonth = 6;
             } else {
                 currentMonth = 9;
             }
         }
         if (flag === 'max') {
-             if ( 0 <= month && month < 3 ) {
-                 currentMonth = 3;
-             } else if ( month <= 6) {
-                 currentMonth = 6;
-             } else if ( month <= 9) {
-                 currentMonth = 9;
-             }
+            if (0 <= month && month < 3) {
+                currentMonth = 3;
+            } else if (month <= 6) {
+                currentMonth = 6;
+            } else if (month <= 9) {
+                currentMonth = 9;
+            }
         }
 
-       date.setMonth(currentMonth);
-       date.setDate(1);
-       date.setHours(0, 0, 0, 0);
-       return date;
-   }
-   
+        date.setMonth(currentMonth);
+        date.setDate(1);
+        date.setHours(0, 0, 0, 0);
+        return date;
+    }
 
-   if (picker === 'year') {
-       date.setMonth(0);
-       date.setDate(1);
-       date.setHours(0, 0, 0, 0);
-       return date;
-   }
+    if (picker === 'year') {
+        date.setMonth(0);
+        date.setDate(1);
+        date.setHours(0, 0, 0, 0);
+        return date;
+    }
 
-   return currentDate;
+    return currentDate;
 }
