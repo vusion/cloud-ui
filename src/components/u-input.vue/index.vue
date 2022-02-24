4979edd378a092de3c02493bd96ab98bb8079848
Bener <benjavan@163.com>
2022-02-24 21:55:33 +0800

    feat: 密码输入框支持切换密码显隐

--- src/components/u-input.vue/index.vue
+++ src/components/u-input.vue/index.vue
@@ -1,17 +1,19 @@
 <template>
 <div :class="$style.root" :readonly="readonly" :disabled="disabled" :color="currentColor || formItemVM && formItemVM.color"
     :focus="focused" :clearable="clearable && currentValue" :prefix="prefix" :suffix="suffix"
+    :show-password="showPassword" :password="password"
     @click.self="!focused && focus()">
     <span :class="$style.baseline">b</span><!-- 用于基线对齐 -->
     <span :class="$style.placeholder">{{ currentValue ? '' : placeholder }}</span><!-- 兼容 IE9 -->
     <span v-if="prefix" :class="$style.prefix" :name="prefix" @click="$emit('click-prefix', $event, this)"><slot name="prefix"></slot></span>
-    <input ref="input" :class="$style.input" v-bind="$attrs" :value="currentValue"
+    <input ref="input" :class="$style.input" v-bind="$attrs" :type="curType" :value="currentValue"
         :placeholder="placeholder" v-focus="autofocus" :readonly="readonly" :disabled="disabled"
         @input="onInput" @focus="onFocus" @blur="onBlur" @keypress="onKeypress" @keyup="onKeyup" v-on="listeners"
         @compositionstart="compositionInputing = true"
         @compositionend="onCompositionEnd"
         :title="currentValue && !disabled ? '' : ($attrs.title || placeholder)">
     <slot></slot>
+    <span :class="$style.password" v-if="password" @click.stop="togglePassword"></span>
     <span v-if="suffix" v-show="!clearable || !currentValue" :class="$style.suffix" :name="suffix"
         @click="$emit('click-suffix', $event, this)"><slot name="suffix"></slot></span>
     <span :class="$style.clearable" v-if="clearable && currentValue" @click.stop="clear"></span>
@@ -34,6 +36,8 @@ export default {
         autofocus: { type: [Boolean, String], default: false },
         readonly: { type: Boolean, default: false },
         disabled: { type: Boolean, default: false },
+        password: { type: Boolean, default: false },
+        type: { type: String, default: 'text' },
         maxlengthMessage: String,
         prefix: String,
         suffix: String,
@@ -49,6 +53,8 @@ export default {
             currentValue: this.value,
             currentColor: this.color,
             compositionInputing: false,
+            showPassword: false,
+            curType: this.type,
         };
     },
     computed: {
@@ -66,6 +72,14 @@ export default {
             });
             return listeners;
         },
+        showPasswordBtn() {
+            return (
+                this.type === 'password'
+                && this.password
+                && !this.disabled
+                && !this.readonly
+            );
+        },
     },
     watch: {
         value(value) {
@@ -78,6 +92,9 @@ export default {
         },
         color(color) {
             this.currentColor = color;
+        },
+        type(type) {
+            this.curType = type;
         },
     },
     mounted() {
@@ -160,22 +177,29 @@ export default {
             this.focus();
             this.$emit('clear', $event, this);
         },
+        togglePassword() {
+            if (this.readonly || this.disabled)
+                return;
+            const showPassword = this.curType === 'text';
+            this.showPassword = !showPassword;
+            this.curType = showPassword ? 'password' : 'text';
+        },
         autoResize() {
             const inputEl = this.$refs.input;
             if (this.autoSize === 'both' || this.autoSize === 'horizontal') {
                 inputEl.style.width = '3px';
                 this.$el.style.width
                     = inputEl.scrollWidth
-                        + (this.$el.offsetWidth - this.$el.clientWidth)
-                        + 'px';
+                    + (this.$el.offsetWidth - this.$el.clientWidth)
+                    + 'px';
                 inputEl.style.width = '';
             }
             if (this.autoSize === 'both' || this.autoSize === 'vertical') {
                 inputEl.style.height = '3px';
                 this.$el.style.height
                     = inputEl.scrollHeight
-                        + (this.$el.offsetHeight - this.$el.clientHeight)
-                        + 'px';
+                    + (this.$el.offsetHeight - this.$el.clientHeight)
+                    + 'px';
                 inputEl.style.height = '';
             }
         },
