e7c23eb27841c8808e2544e00266cf06321bc2da
violarong <zxrong@gmail.com>
2022-03-15 14:34:01 +0800

    :sparkles: u-rate半星功能

--- src/components/u-rate.vue/docs/examples.md
+++ src/components/u-rate.vue/docs/examples.md
@@ -15,6 +15,40 @@ export default {
 </script>
 ```
 
+### 允许半选
+
+``` vue
+<template>
+<u-rate v-model="rating" :allow-half="true"></u-rate>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            rating: 3,
+        };
+    },
+};
+</script>
+```
+
+### 允许清除
+
+``` vue
+<template>
+<u-rate v-model="rating" :allow-clear="true"></u-rate>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            rating: 3,
+        };
+    },
+};
+</script>
+```
+
 ### 只读
 
 ``` vue
@@ -26,6 +60,23 @@ export default {
     data() {
         return {
             rating: 3,
+        };
+    },
+};
+</script>
+```
+
+### 只读，值为小数
+
+``` vue
+<template>
+<u-rate :value="rating" readonly></u-rate>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            rating: 3.6,
         };
     },
 };
--- src/components/u-rate.vue/index.vue
+++ src/components/u-rate.vue/index.vue
@@ -1,8 +1,12 @@
 <template>
-<div :class="$style.root" @mouseleave="onMouseleave">
-    <span :class="$style.item" v-for="i in max" :key="i" :status="i <= (isHover ? hoverIndex : currentValue) ? 'full' : ''"
+<div :class="$style.root" @mouseleave="onMouseleave" :readonly="readonly">
+    <span :class="$style.item" v-for="i in max" :key="i" :status="getFullStatus(i)"
         @mousemove="onMousemove(i, $event)"
-        @click="select(i)"></span>
+        @click="select(i)" :half="getHalfStatus(i)"
+        ref="star">
+        <span :class="$style.mask" :style="{width: decimalWidth + '%'}"></span>
+    </span>
+    <span :class="$style.text" v-if="showText">{{ tooltips[tooltipIndex] }}</span>
 </div>
 </template>
 
@@ -16,8 +20,35 @@ export default {
         value: { type: Number, default: 0 },
         max: { type: Number, default: 5 },
         readonly: { type: Boolean, default: false },
-        showText: { type: Boolean, default: false },
         texts: { type: Array },
+        allowHalf: { type: Boolean, default: false },
+        clearable: { type: Boolean, default: false },
+        showText: { type: Boolean, default: false },
+        showTips: { type: Boolean, default: false },
+        tooltips: {
+            type: Array,
+            default() {
+                return ['极差', '失望', '一般', '满意', '惊喜'];
+            },
+        },
+    },
+    computed: {
+        decimalWidth() {
+            if (this.allowHalf) {
+                return 50;
+            } else {
+                return this.currentValue * 100 - Math.floor(this.currentValue) * 100;
+            }
+        },
+        tooltipIndex(){
+            if (this.isHover) {
+                return this.hoverIndex - 1;
+            } else {
+                const value = Math.floor(this.currentValue);
+                return value - 1;
+            }
+        }
+
     },
     data() {
         const list = [];
@@ -28,6 +59,7 @@ export default {
             list,
             isHover: false,
             hoverIndex: -1,
+            pointerAtLeftHalf: false,
         };
     },
     watch: {
@@ -46,10 +78,15 @@ export default {
             if (this.readonly) {
                 return false;
             }
+            if (this.allowHalf) {
+                const target = this.$refs.star[i-1];
+                this.pointerAtLeftHalf = $event.offsetX * 2 <= target.clientWidth;
+            }
             this.isHover = true;
             this.hoverIndex = i;
         },
         onMouseleave() {
+            this.pointerAtLeftHalf = false;
             this.isHover = false;
             this.hoverIndex = -1;
         },
@@ -57,16 +94,53 @@ export default {
             if (this.readonly) {
                 return false;
             }
-            this.currentValue = i;
-            this.$emit('input', i, this);
-            this.$emit('update:value', i, this);
+            const newValue = this.pointerAtLeftHalf? i - 0.5 :i;
+            let isReset = false;
+            if (this.clearable) {
+                isReset = this.currentValue === newValue;
+            }
+            this.onMouseleave();
+            this.currentValue = isReset? 0 : newValue;
+            this.$emit('input', this.currentValue, this);
+            this.$emit('update:value', this.currentValue, this);
         },
+        showMask(index){
+            const allowDecimal = this.readonly && this.currentValue > index -1 && index > this.currentValue;
+            const allowClickHalf = this.allowHalf && this.pointerAtLeftHalf && this.hoverIndex === index;
+            console.log('showMask', allowDecimal || allowClickHalf);
+            return allowDecimal || allowClickHalf;
+        },
+        getFullStatus(index) {
+            let isFull = false;
+            if(this.isHover) {
+                if(this.pointerAtLeftHalf) {
+                    isFull = index <= this.hoverIndex - 0.5;
+                } else {
+                    isFull = index <= this.hoverIndex;
+                }
+            } else {
+                isFull = index<= this.currentValue;
+            }
+            return isFull? 'full' : '';
+        },
+        getHalfStatus(index) {
+            if (this.isHover) {
+                return this.hoverIndex === index && this.pointerAtLeftHalf;
+            } else {
+                return this.currentValue > index -1 && this.currentValue < index;
+            }
+        }
     },
 };
 </script>
 
 <style module>
-.root {}
+.root[readonly] .item {
+    cursor: default;
+}
+.root[readonly] .item:hover{
+    transform: scale(1);
+}
 
 .item {
     display: inline-block;
@@ -74,6 +148,7 @@ export default {
     transition: all .3s ease;
     color: var(--rate-color);
     cursor: var(--cursor-pointer);
+    position: relative;
 }
 
 .item:hover {
@@ -90,5 +165,27 @@ export default {
 
 .item[status="full"] {
     color: var(--rate-color-full);
+}
+.item[half] .mask {
+    opacity: 1;
+}
+
+.mask {
+    position: absolute;
+    height: 100%;
+    left: 0;
+    overflow: hidden;
+    color: var(--rate-color-full);
+    opacity: 0;
+    width: 50%;
+    transition: all .3s;
+}
+.mask::before {
+    icon-font: url('./assets/star.svg');
+}
+
+.text {
+    vertical-align: 2px;
+    color: var(--rate-text-color);
 }
 </style>
