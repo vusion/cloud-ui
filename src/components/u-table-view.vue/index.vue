6f68be51c625d5755f490941267b120ebf6e5929
hzlingqiao <hzlingqiao@corp.netease.com>
2022-03-15 09:54:17 +0800

    fix: 强制更新dom

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -555,6 +555,9 @@ export default {
             this.currentDataSource = this.normalizeDataSource(this.dataSource || this.data);
             this.initialLoad && this.load();
             this.handleResize();
+            this.$nextTick(() => {
+                this.$forceUpdate();
+            });
         },
         getExtraParams() {
             return this.extraParams;
