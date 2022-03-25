dce00964f93fa655f79ca1e3eaf0f412adeeecc4
violarong <zxrong@gmail.com>
2022-03-25 10:49:34 +0800

    :bug: 搜索下拉框分页数据加载错误，选中未清除

--- src/components/u-select.vue/index.vue
+++ src/components/u-select.vue/index.vue
@@ -236,6 +236,9 @@ export default {
         this.toggle(this.opened);
         this.setPopperWidth();
     },
+    destroyed() {
+        clearTimeout(this.inputDeleteTimer);
+    },
     methods: {
         getExtraParams() {
             return { filterText: this.filterText };
@@ -437,14 +440,18 @@ export default {
             this.popperOpened ? this.close() : this.open();
         },
         onInputDelete() {
-            if (this.filterable && this.filterText === '') {
-                if (!this.selectedVMs.length)
-                    return;
-                const lastItemVM = this.selectedVMs[
-                    this.selectedVMs.length - 1
-                ];
-                this.select(lastItemVM, false);
-            }
+            // 增加setTimeout原因：没有setTimeout，该函数会在onInput前执行，filterText会差一个字母
+            this.inputDeleteTimer = setTimeout(()=>{
+                if (this.filterable && this.filterText === '') {
+                    this.selectedVM = undefined; // 清空时清除下拉选中项
+                    if (!this.selectedVMs.length)
+                        return;
+                    const lastItemVM = this.selectedVMs[
+                        this.selectedVMs.length - 1
+                    ];
+                    this.select(lastItemVM, false);
+                }
+            }, 0);
         },
         clear() {
             this.preventBlur = true;
