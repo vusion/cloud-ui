8585b4dc9773d7faf0e5faa6ff1d8a883c936970
linghaha <lingqiao1994@126.com>
2022-04-01 11:52:01 +0800

    chore: 调整脚本和theme.css

--- scripts/genThemeConfig/index.ts
+++ scripts/genThemeConfig/index.ts
@@ -1,63 +1,85 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as postcss from 'postcss';
 
-export interface ThemeVariables {
-    [name: string]: string,
-};
-
-export interface ThemeProperty {
-    type: 'input' | 'color';
-    default: string;
-};
-
-export interface ThemeComponent {
-    name: string;
-    title?: string;
-    properties: {
-        [name: string]: ThemeProperty,
-    };    
-}
-
-const themeComponents: Array<ThemeComponent> = [];
+const themeComponentsMap: any = {};
 
 const cssContent = fs.readFileSync(path.join(__dirname, '../../src/styles/theme.css'), 'utf-8');
 const root = postcss.parse(cssContent);
 const _root = root.nodes.find((node) => node.type === 'rule' && node.selector === ':root') as postcss.Rule;
 
-let lastComponent: ThemeComponent;
+let lastComponent: any;
 let lastProp: string;
+
 _root.nodes.forEach((node) => {
-    if (node.type === 'comment' && (node.raws.before && node.raws.before.includes('\n')) && node.text.includes('@component ')) {
-        const cap = /@component\s+([\w-]+)(\s+@hidden)?/.exec(node.text.trim());
-        const name = cap[1].trim();
-        const hidden = !!cap[2];
+    if (node.type === 'comment') {
+        if (node.raws.before && node.raws.before.includes('\n')) {
+            if (node.text.includes('@component ')) {
+                const cap = /@component\s+([\w-]+)(\s+@hidden)?/.exec(node.text.trim());
+                const name = cap[1].trim();
+                const hidden = !!cap[2];
 
-        if ((!lastComponent || lastComponent.name !== name)) {
-            if (hidden) {
-                return lastComponent = undefined;
+                if ((!lastComponent || lastComponent.name !== name)) {
+                    if (lastComponent) {
+                        const {
+                            name: componentName,
+                            cssProperty
+                        } = lastComponent;
+                        themeComponentsMap[componentName] = cssProperty;
+                    }
+
+                    lastComponent = {
+                        name,
+                        cssProperty: {}
+                    }
+                    if (hidden) {
+                        lastComponent = undefined;
+                    }
+                }
             }
-
-            lastComponent && themeComponents.push(lastComponent);
-            lastComponent = {
-                name,
-                properties: {}
+        } else if (lastComponent) {
+            if (node.text.trim() === '@hidden') {
+                delete lastComponent.cssProperty[lastProp];
+            } else if (node.text.includes('@type ')) {
+                const cap = /@type\s+([\w-]+)/.exec(node.text.trim());
+                lastComponent.cssProperty[lastProp] = cap[1].trim();
             }
         }
-    } else if (node.type === 'comment' && (!node.raws.before || !node.raws.before.includes('\n')) && node.text.trim() === '@hidden') {
-        delete lastComponent.properties[lastProp];
     } else if (node.type === 'decl') {
         if (!lastComponent)
             return;
-        lastComponent.properties[node.prop] = {
-            type: 'input',
-            default: node.value,
-        };
+        lastComponent.cssProperty[node.prop] = 'input';
         lastProp = node.prop;
     }
 });
-lastComponent && themeComponents.push(lastComponent);
+if (lastComponent) {
+    const {
+        name: componentName,
+        cssProperty
+    } = lastComponent;
+    themeComponentsMap[componentName] = cssProperty;
+}
 
-const result = JSON.stringify(themeComponents, null, 4)
-    .replace(/\{\n\s+"type": "(.+?)",\s+"default": "(.+?)"\s+}/g, `{ "type": "$1", "default": "$2" }`);
-fs.writeFileSync(path.join(__dirname, './result.json'), result, 'utf8');
+const resultPath = path.join(__dirname, './result.json');
+const resultList = fs.readJSONSync(resultPath);
+
+if(Array.isArray(resultList)) {
+    resultList.forEach((result) => {
+        const { toPosition, items } = result || {};
+        if(toPosition === 'highLevelSetting') {
+            if(Array.isArray(items)) {
+                items.forEach((item) => {
+                    const { name } = item || {};
+                    const cssProperty = themeComponentsMap[name];
+                    if(cssProperty) {
+                        item.cssProperty = cssProperty;
+                    }
+                });
+            }
+        }
+    })
+}
+
+fs.writeJsonSync(resultPath, resultList, {
+    spaces: 4
+});
