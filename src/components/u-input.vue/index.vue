8c2341d0647ed8a433a32da3ddfab9164e9ab332
daysai <wddpwzz36@gmail.com>
2022-04-01 20:36:25 +0800

    :bug: add valueEmpty for u-input

--- src/components/u-input.vue/index.vue
+++ src/components/u-input.vue/index.vue
@@ -3,18 +3,18 @@
     :focus="focused" :clearable="clearable && currentValue" :prefix="prefix" :suffix="suffix"
     @click.self="!focused && focus()">
     <span :class="$style.baseline">b</span><!-- 用于基线对齐 -->
-    <span :class="$style.placeholder" v-show="placeholder">{{ showPlaceholder ? placeholder : ''}}</span><!-- 兼容 IE9 -->
+    <span :class="$style.placeholder" v-show="placeholder">{{ valueEmpty ? placeholder : ''}}</span><!-- 兼容 IE9 -->
     <span v-if="prefix" :class="$style.prefix" :name="prefix" @click="$emit('click-prefix', $event, this)"><slot name="prefix"></slot></span>
     <input ref="input" :class="$style.input" v-bind="$attrs" :value="currentValue"
         v-focus="autofocus" :readonly="readonly" :disabled="disabled"
         @input="onInput" @focus="onFocus" @blur="onBlur" @keypress="onKeypress" @keyup="onKeyup" v-on="listeners"
         @compositionstart="compositionInputing = true"
         @compositionend="onCompositionEnd"
-        :title="!showTitle || (currentValue && !disabled) ? null : ($attrs.title || placeholder)">
+        :title="!showTitle || (!valueEmpty && !disabled) ? null : ($attrs.title || placeholder)">
     <slot></slot>
-    <span v-if="suffix" v-show="!clearable || !currentValue" :class="$style.suffix" :name="suffix"
+    <span v-if="suffix" v-show="!clearable || valueEmpty" :class="$style.suffix" :name="suffix"
         @click="$emit('click-suffix', $event, this)"><slot name="suffix"></slot></span>
-    <span :class="$style.clearable" v-if="clearable && currentValue" @click.stop="clear"></span>
+    <span :class="$style.clearable" v-if="clearable && !valueEmpty" @click.stop="clear"></span>
 </div>
 </template>
 
@@ -67,7 +67,7 @@ export default {
             });
             return listeners;
         },
-        showPlaceholder() {
+        valueEmpty() {
             const { currentValue } = this;
             return currentValue === undefined || currentValue === '' || currentValue === null;
         },
