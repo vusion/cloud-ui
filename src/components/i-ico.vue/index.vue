f95ed267db4f03d9d74ea30526abf16e0ee86244
linghaha <lingqiao1994@126.com>
2022-07-06 22:28:23 +0800

    fix: 对encodeUrl url为空的情况处理

--- src/components/i-ico.vue/index.vue
+++ src/components/i-ico.vue/index.vue
@@ -56,15 +56,15 @@ export default {
             const parent = this.$parent;
             function currentHref() {
                 if (props.href !== undefined)
-                    return props.href;
+                    return encodeUrl(props.href);
                 if (props.destination !== undefined && props.destination !== "")
-                    return props.destination;
+                    return encodeUrl(props.destination);
                 else if (parent && parent.$router && props.to !== undefined)
-                    return parent && parent.$router.resolve(props.to, parent && parent.$route, props.append).href;
+                    return encodeUrl(parent && parent.$router.resolve(props.to, parent && parent.$route, props.append).href);
                 else
                     return undefined;
             }
-            const hrefR = encodeUrl(currentHref());
+            const hrefR = currentHref();
             this.$emit('click', ev);
             // @ts-ignore：没办法
             // if (props.target !== '_self')
