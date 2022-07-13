db7b2b21826c2306ae74cc70f1ad16e92e4bc8e6
zhognzichen <zhongzichen@cn.net.ntes>
2022-07-13 15:21:05 +0800

    fix:cascader组件代码规范化

--- src/components/u-cascader.vue/item.vue
+++ src/components/u-cascader.vue/item.vue
@@ -19,110 +19,107 @@
 import { findScrollParent } from '../../utils/dom';
 
 export default {
-    name: "u-cascader-item",
+    name: 'u-cascader-item',
     props: {
         data: { type: Array, default: () => [] },
-        field: { type: String, default: "text"},
-        trigger: { type: String, default: "click"},
-        lazy: { type: Boolean, default: false},
-        componentIndex: Number,     //第几个cascaderitem组件
-        selectSubIdnex: Number,     //parent选择了第几个cascaderitem组件
+        field: { type: String, default: 'text' },
+        trigger: { type: String, default: 'click' },
+        lazy: { type: Boolean, default: false },
+        componentIndex: Number, // 第几个cascaderitem组件
+        selectSubIdnex: Number, // parent选择了第几个cascaderitem组件
         isInput: Boolean,
     },
-    data(){
+    data() {
         return {
             umenuIndex: -1,
-        }
-    },
-    mounted(){
-        this.addMouseenter(this.data, 0);
+        };
     },
     watch: {
-        selectSubIdnex(newValue){
-            if(this.componentIndex > newValue)
+        selectSubIdnex(newValue) {
+            if (this.componentIndex > newValue)
                 this.umenuIndex = -1;
         },
-        data(newdata, predata){
-            if(newdata.length > predata.length)
+        data(newdata, predata) {
+            if (newdata.length > predata.length)
                 this.$nextTick(() => {
                     this.addMouseenter(newdata.slice(predata.length), predata.length);
-                })
-        }
+                });
+        },
+    },
+    mounted() {
+        this.addMouseenter(this.data, 0);
     },
     methods: {
-        clickMenuitem(selectItem, index){
-            this.selectMenuitem(index)
-            this.$emit("select-umenuitem",selectItem,this.componentIndex);
-            if(this.lazy){
-                if(selectItem.leaf)
-                    this.$emit("select-lastvalue");
-            }
-            else if(!selectItem.children) {
-                this.$emit("select-lastvalue");
+        clickMenuitem(selectItem, index) {
+            this.selectMenuitem(index);
+            this.$emit('select-umenuitem', selectItem, this.componentIndex);
+            if (this.lazy) {
+                if (selectItem.leaf)
+                    this.$emit('select-lastvalue');
+            } else if (!selectItem.children) {
+                this.$emit('select-lastvalue');
             }
         },
-        hoverSelect(selectItem, level){
-            this.selectMenuitem(level)
-            this.$emit("select-umenuitem",selectItem,this.componentIndex);
+        hoverSelect(selectItem, level) {
+            this.selectMenuitem(level);
+            this.$emit('select-umenuitem', selectItem, this.componentIndex);
         },
-        addMouseenter(newDatas, length){
-            if(this.trigger.toLowerCase() === "hover"){
+        addMouseenter(newDatas, length) {
+            if (this.trigger.toLowerCase() === 'hover') {
                 newDatas.forEach((_, index) => {
-                    let currentIndex = length + index;
+                    const currentIndex = length + index;
                     // v-for中使用ref，vue的内部逻辑会自动将ref生成数组，所以要添一个xxx[0]
-                    this.$refs[currentIndex][0].$el.addEventListener("mouseenter",() => {
-                        if(!newDatas[index].disabled)
+                    this.$refs[currentIndex][0].$el.addEventListener('mouseenter', () => {
+                        if (!newDatas[index].disabled)
                             this.hoverSelect(newDatas[index], currentIndex);
-                    })
-                })
+                    });
+                });
             }
         },
-        selectMenuitem(level){
-            if(level === -1)
+        selectMenuitem(level) {
+            if (level === -1)
                 return;
 
             this.umenuIndex = level;
             this.ensureFocusedInView();
         },
-        ensureFocusedInView(){
-            let focusedEl = this.$refs[this.umenuIndex][0].$el;
+        ensureFocusedInView() {
+            const focusedEl = this.$refs[this.umenuIndex][0].$el;
             let parentEl = focusedEl.parentElement;
             parentEl = findScrollParent(focusedEl);
 
-            if(parentEl){
-                if (parentEl.scrollTop < focusedEl.offsetTop + focusedEl.offsetHeight - parentEl.clientHeight){
+            if (parentEl) {
+                if (parentEl.scrollTop < focusedEl.offsetTop + focusedEl.offsetHeight - parentEl.clientHeight) {
                     parentEl.scrollTop = focusedEl.offsetTop
-                                        + focusedEl.offsetHeight
-                                        - parentEl.clientHeight;
+                        + focusedEl.offsetHeight
+                        - parentEl.clientHeight;
                 }
-                if(parentEl.scrollTop > focusedEl.offsetTop)
+                if (parentEl.scrollTop > focusedEl.offsetTop)
                     parentEl.scrollTop = focusedEl.offsetTop;
             }
         },
-        keyboardShift(count, enter){
+        keyboardShift(count, enter) {
             let newUmenuIndex = this.umenuIndex + count;
-            while(this.data[newUmenuIndex] && this.data[newUmenuIndex].disabled){
+            while (this.data[newUmenuIndex] && this.data[newUmenuIndex].disabled) {
                 newUmenuIndex += count;
             }
-            if(newUmenuIndex < 0 || newUmenuIndex >= this.data.length)
-                return
-            let selectItem = this.data[newUmenuIndex];
+            if (newUmenuIndex < 0 || newUmenuIndex >= this.data.length)
+                return;
+            const selectItem = this.data[newUmenuIndex];
             this.selectMenuitem(newUmenuIndex);
-            this.$emit("select-umenuitem", selectItem, this.componentIndex);
-            if(enter && !selectItem.children)
-                this.$emit("select-lastvalue");
+            this.$emit('select-umenuitem', selectItem, this.componentIndex);
+            if (enter && !selectItem.children)
+                this.$emit('select-lastvalue');
         },
-        hasSub(item){
+        hasSub(item) {
             let show = false;
-            if(item.children || ('leaf' in item && !item.leaf))
-                if(!item.loading)
+            if (item.children || ('leaf' in item && !item.leaf))
+                if (!item.loading)
                     show = true;
             return show;
-        }
-    }
+        },
+    },
 };
 </script>
-
-
 
 <style module src="./item.css"> </style>
