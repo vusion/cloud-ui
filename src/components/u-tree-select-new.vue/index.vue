e30e933e5ad40d8611e9e599b89a3a9f03ff4389
zanningwang <wangzanning@me.com>
2022-09-03 22:10:27 +0800

    fix: 修复树选择框选中不收回和目录禁用无效的问题

--- src/components/u-tree-select-new.vue/index.vue
+++ src/components/u-tree-select-new.vue/index.vue
@@ -100,16 +100,11 @@
                 :filterFields="filterFields"
                 @change="$emit('change', $event, this)"
                 @before-select="$emit('before-select', $event, this)"
-                @select="$emit('select', $event, this)"
+                @select="$emit('select', $event, this) && close()"
                 @input="$emit('input', $event, this)"
                 @update:value="onUpdateValue"
                 @toggle="$emit('toggle', $event, this)"
                 @check="$emit('check', $event, this)">
-                <template v-if="dataSource && $env.VUE_APP_DESIGNER">
-                    <u-tree-view-node-new :text="scopeItem" readonly></u-tree-view-node-new>
-                    <u-tree-view-node-new :text="scopeItem" disabled></u-tree-view-node-new>
-                    <u-tree-view-node-new :text="scopeItem" disabled></u-tree-view-node-new>
-                </template>
                 <template #text="props">
                     <slot name="text" v-bind="props">{{ props.text }}</slot>
                 </template>
