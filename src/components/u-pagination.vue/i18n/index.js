298a5653ddea764fd9f1bab386faae7e3405b4cf
Bener <benjavan@163.com>
2022-11-16 22:38:58 +0800

    feat: 分页器支持日语

--- src/components/u-pagination.vue/i18n/index.js
+++ src/components/u-pagination.vue/i18n/index.js
@@ -1,9 +1,11 @@
 import enUS from './en-US';
 import zhCN from './zh-CN';
+import ja from './ja';
 
 export default {
     messages: {
         'en-US': enUS,
         'zh-CN': zhCN,
+        ja,
     },
 };
