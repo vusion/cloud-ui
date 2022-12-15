7626fcf267a9d0532c0bfa388bb042422b93d5b9
zanningwang <wangzanning@me.com>
2022-12-15 14:22:54 +0800

    🐛 ULink destination解析打包报错

--- src/components/u-link.vue/index.vue
+++ src/components/u-link.vue/index.vue
@@ -104,7 +104,7 @@ export default {
                         return;
                         // 处理同页面锚点跳转
                     } else if (this.destination.indexOf('#') !== -1 && beforeHashUrl === location.pathname) {
-                        const hash = this.destination.slice(this.destination.indexOf('#'))?.replace('#', '');
+                        const hash = this.destination.slice(this.destination.indexOf('#')) && this.destination.slice(this.destination.indexOf('#')).replace('#', '');
                         if (document.getElementById(hash)) {
                             document.getElementById(hash).scrollIntoView();
                             // this.$router.push(this.destination);
