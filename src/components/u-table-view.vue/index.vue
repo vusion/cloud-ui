8d8ace52da05e861eeb26b369bf1a01c091cefcf
daysai <wddpwzz36@gmail.com>
2023-05-09 14:46:44 +0800

    :bug: fix dataSource not support ListTotal

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -833,6 +833,10 @@ export default {
                 }
                 return new Constructor(options);
             } else if (dataSource instanceof Object) {
+                if (dataSource.hasOwnProperty('list') && Array.isArray(dataSource.list))
+                    return new Constructor(Object.assign(options, dataSource, {
+                        data: dataSource.list,
+                    }));
                 return new Constructor(Object.assign(options, dataSource));
             } else
                 return dataSource;
