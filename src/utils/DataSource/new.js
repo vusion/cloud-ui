51d0637fc06e5895a50d85e5b5a22bb312c78afb
jiyufeng <18012261618@126.com>
2023-05-17 11:24:34 +0800

    fix: 修复数据表格树形父节点报错

--- src/utils/DataSource/new.js
+++ src/utils/DataSource/new.js
@@ -442,17 +442,16 @@ const VueDataSource = Vue.extend({
             const tree = [];
             data.forEach((item) => {
                 const parentId = this.$at(item, parentField);
-                if (!parentId) {
+                const parent = nodes[parentId];
+                // 没有parentId 或者 parent不存在的不处理
+                if (!parentId || !parent) {
                     tree.push(item);
                 } else {
-                    const parent = nodes[parentId];
-                    if (parent) {
-                        if (!this.$at(parent, childrenField)) {
-                            this.$setAt(parent, childrenField, []);
-                        }
-
-                        this.$at(parent, childrenField).push(item);
+                    if (!this.$at(parent, childrenField)) {
+                        this.$setAt(parent, childrenField, []);
                     }
+
+                    this.$at(parent, childrenField).push(item);
                 }
             });
 
