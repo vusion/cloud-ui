5c855807a410c0abe16b1ca18ed7f7b03a600a5c
Fuquan <lifuquan1985@hotmail.com>
2023-05-19 15:52:56 +0800

    fix: 日历组件日期问题

--- src/components/u-calendar-view.vue/utils.js
+++ src/components/u-calendar-view.vue/utils.js
@@ -5,8 +5,6 @@ export const DefaultFormatType = 'YYYY-MM-DD';
 export function getDay(value, defaultValue) {
     if (!value)
         return defaultValue;
-    else if (typeof value === 'string')
-        return dayjs(value, DefaultFormatType);
     else
         return dayjs(value);
 }
