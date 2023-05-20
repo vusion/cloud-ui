8f3436fc81c544b89341e497af191149671e1a47
violarong <zxrong@gmail.com>
2023-05-20 17:25:26 +0800

    feat 级联组件数据源优化

---
+++ src/mixins/tree.datasource.js
@@ -0,0 +1,39 @@
+export default {
+    props: {
+        parentField: String,
+        childrenField: { type: String, default: 'children' },
+        valueField: { type: String, default: 'value' },
+    },
+    methods: {
+        listToTree(data) {
+            const { valueField, parentField, childrenField } = this;
+
+            // Map记录一下
+            const nodes = {}; // Record<id, { entity }>
+            data.forEach((item) => {
+                const id = this.$at(item, valueField);
+                if (id) {
+                    nodes[id] = item;
+                }
+            });
+
+            const tree = [];
+            data.forEach((item) => {
+                const parentId = this.$at(item, parentField);
+                const parent = nodes[parentId];
+                // 没有parentId 或者 parent不存在的不处理
+                if (!parentId || !parent) {
+                    tree.push(item);
+                } else {
+                    if (!this.$at(parent, childrenField)) {
+                        this.$setAt(parent, childrenField, []);
+                    }
+
+                    this.$at(parent, childrenField).push(item);
+                }
+            });
+
+            return tree;
+        },
+    },
+};
