aa40972867c8344a4495b525afd2b71dfbfa8b1a
ncqwer <1732031236@qq.com>
2023-08-18 16:12:40 +0800

    fix: UNavbarMulti修复测试错误

--- src/components/u-navbar-multi.vue/index.vue
+++ src/components/u-navbar-multi.vue/index.vue
@@ -123,9 +123,9 @@ export default {
             if (this.$slots.default && this.$slots.default.length > 0) {
                 let temp = [];
                 this.$slots.default.forEach((wrapVnode) => {
-                    if (wrapVnode.tag.endsWith('u-multi-layout-item')) {
-                        temp = temp.concat(wrapVnode.componentOptions.children.filter((vnode) => vnode.tag.endsWith('u-navbar-item-multi')));
-                    } else if (wrapVnode.tag.endsWith('u-navbar-item-multi') || wrapVnode.tag.endsWith('u-navbar-group-multi')) {
+                    if (wrapVnode.tag && wrapVnode.tag.endsWith('u-multi-layout-item')) {
+                        temp = temp.concat(wrapVnode.componentOptions.children.filter((vnode) => vnode.tag && vnode.tag.endsWith('u-navbar-item-multi')));
+                    } else if (wrapVnode.tag && (wrapVnode.tag.endsWith('u-navbar-item-multi') || wrapVnode.tag.endsWith('u-navbar-group-multi'))) {
                         temp = [...temp, wrapVnode];
                     } else {
                         console.error(`[u-navbar-multi] 目前仅支持[u-multi-layout-item],[u-navbar-group-multi]和[u-navbar-item-multi]作为默认插槽的内容`);
