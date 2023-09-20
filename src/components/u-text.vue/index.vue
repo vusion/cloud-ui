5b15de1278fe699db9d157c47bd778ede266a811
御风 <18012261618@126.com>
2023-09-20 16:11:24 +0800

    feat: 文本支持格式化

--- src/components/u-text.vue/index.vue
+++ src/components/u-text.vue/index.vue
@@ -1,14 +1,179 @@
 <template>
 <span :class="$style.root" v-on="$listeners" vusion-slot-name-edit="text">
-    <slot>{{ text }}</slot>
+    <slot>{{ formatedText }}</slot>
 </span>
 </template>
 
 <script>
+import dayjs from '../../utils/dayjs';
+
+const validFormatType = ['none', 'number', 'percentage', 'date', 'time', 'datetime'];
+
 export default {
     name: 'u-text',
     props: {
         text: String,
+        format: Object,
+    },
+    computed: {
+        isValidFormat() {
+            if (!this.format) {
+                return false;
+            }
+
+            const { type, options } = this.format;
+
+            if (!validFormatType.includes(type)) {
+                return false;
+            }
+
+            if (type === 'none') {
+                return false;
+            }
+
+            if (type === 'number') {
+                const { decimal, thousandths, percentage, unit } = options || {};
+                return decimal?.places || thousandths || percentage || unit.value;
+            }
+
+            if (type === 'percentage') {
+                const { decimal, thousandths } = options || {};
+                return decimal?.places || thousandths;
+            }
+
+            if (type === 'date') {
+                return options?.date;
+            }
+
+            if (type === 'time') {
+                return options?.time;
+            }
+
+            if (type === 'datetime') {
+                return options?.datetime;
+            }
+
+            return false;
+        },
+        // 格式化后的文本
+        formatedText() {
+            if (!this.isValidFormat) {
+                return this.text;
+            }
+
+            const { type, options } = this.format;
+
+            if (type === 'number') {
+                let num = Number(this.text);
+                // 文本是否能转number
+                if (isNaN(num)) {
+                    return this.text;
+                }
+
+                const { decimal, thousandths, percentage, unit } = options;
+                // 百分比
+                if (percentage) {
+                    num *= 100;
+                }
+
+                // 小数位
+                if (decimal?.places && decimal?.places !== '') {
+                    num = num.toFixed(decimal?.places);
+
+                    if (decimal?.omit) {
+                        num = Number(num);
+                    }
+                }
+
+                // 千分符
+                if (thousandths) {
+                    const [left, right] = num.toString().split('.');
+                    num = [left.replace(/\B(?=(\d{3})+(?!\d))/g, ','), right].filter(Boolean).join('.');
+                }
+
+                // 百分比
+                if (percentage) {
+                    num += '%';
+                }
+
+                if (unit?.value) {
+                    if (unit.type === 'prefix') {
+                        num = `${unit?.value} ${num}`;
+                    } else if (unit.type === 'suffix') {
+                        num = `${num} ${unit?.value}`;
+                    }
+                }
+
+                return num;
+            }
+
+            if (type === 'percentage') {
+                let num = Number(this.text);
+                // 文本是否能转number
+                if (isNaN(num)) {
+                    return this.text;
+                }
+
+                const { decimal, thousandths } = options;
+                // 百分比
+                num *= 100;
+
+                // 小数位
+                if (decimal?.places && decimal?.places !== '') {
+                    num = num.toFixed(decimal?.places);
+
+                    if (decimal?.omit) {
+                        num = Number(num);
+                    }
+                }
+
+                // 千分符
+                if (thousandths) {
+                    const [left, right] = num.toString().split('.');
+                    num = [left.replace(/\B(?=(\d{3})+(?!\d))/g, ','), right].filter(Boolean).join('.');
+                }
+
+                // 百分比
+                num += '%';
+
+                return num;
+            }
+
+            if (type === 'date') {
+                const date = dayjs(this.text);
+                // 是否是
+                if (!date.isValid()) {
+                    return this.text;
+                }
+
+                const { date: formatter } = options;
+                return date.format(formatter);
+            }
+
+            if (type === 'time') {
+                const date = dayjs(this.text, 'HH:mm:ss');
+                // 是否是
+                if (!date.isValid()) {
+                    return this.text;
+                }
+
+                const { time: formatter } = options;
+                return date.format(formatter);
+            }
+
+            if (type === 'datetime') {
+                const date = dayjs(this.text);
+                // 是否是
+                if (!date.isValid()) {
+                    return this.text;
+                }
+
+                const { datetime: formatter } = options;
+                return date.format(formatter);
+            }
+
+            return this.text;
+        },
     },
 };
 </script>
