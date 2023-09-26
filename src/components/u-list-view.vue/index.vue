2c02ad527d07baa7add034c93a7cf65eda2ae96e
wybieshang <shangjian01@corp.netease.com>
2023-09-26 20:31:59 +0800

    fix:修复list-view插槽展示问题

--- src/components/u-list-view.vue/index.vue
+++ src/components/u-list-view.vue/index.vue
@@ -29,8 +29,7 @@
                 :ellipsis-title="ellipsisTitle"
                 :item="item"
                 :index="index">
-                <slot name="item" :item="item" :index="index" :text="$at(item, field || textField) || item" :value="$at(item, valueField) || item" :disabled="item.disabled || disabled" vusion-slot-name="item" :ellipsis-title="ellipsisTitle">{{ isPrimitive(item) ? item : $at(item, field || textField) }}<s-empty v-if="(!$slots.item) && $env.VUE_APP_DESIGNER && !!$attrs['vusion-node-path']"></s-empty>
-            </slot>
+                <slot name="item" :item="item" :index="index" :text="$at(item, field || textField) || item" :value="$at(item, valueField) || item" :disabled="item.disabled || disabled" vusion-slot-name="item" :ellipsis-title="ellipsisTitle">{{ isPrimitive(item) ? item : $at(item, field || textField) }}<s-empty v-if="(!$slots.item) && $env.VUE_APP_DESIGNER && !!$attrs['vusion-node-path']"></s-empty></slot>
             </component>
         </div>
         <div :class="$style.status" status="loading" v-if="currentLoading" vusion-slot-name="loading">
