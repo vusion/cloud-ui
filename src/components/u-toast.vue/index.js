1af736fcb04e7a206080cb3adad4ad9148f5fb71
御风 <18012261618@126.com>
2023-10-18 13:31:48 +0800

    fix: u-toast-single

--- src/components/u-toast.vue/index.js
+++ src/components/u-toast.vue/index.js
@@ -1,9 +1,7 @@
 import UToast from './index.vue';
 import UToastDesigner from './index.designer.vue';
+import UToastSingle from './index.single.vue';
 
-export {
-    UToast,
-    UToastDesigner,
-};
+export { UToast, UToastDesigner, UToastSingle };
 
 export default UToast;
