853a883d7104d360d1225e542f9a0104cd8c965c
ncqwer <zhujianshi123@gmail.com>
2023-10-20 11:24:15 +0800

    Merge pull request #592 from vusion/fix/UTableView-selectedItem-invalid-when-dataSource-change-231020-hanshijie
    fix: 修复在数据源发生变化的时候，selectedItem没有及时更新的问题。

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -640,7 +640,10 @@ export default {
 
             this.handleData();
         },
-        currentData(currentData) {
+        currentData(currentData, oldCurrentData) {
+            if (currentData !== oldCurrentData) {
+                this.selectedItem = undefined;
+            }
             this.watchValue(this.value);
             this.watchValues(this.values);
         },
