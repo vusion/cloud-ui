82661606e056b28d96236715f7ecb7b48fea13fb
wybieshang <shangjian01@corp.netease.com>
2023-10-20 16:25:01 +0800

    feat: 全选反选支持converter

--- src/components/u-checkboxes.vue/index.vue
+++ src/components/u-checkboxes.vue/index.vue
@@ -8,10 +8,10 @@
         :value="all"
         :disabled="disabled"
         :readonly="readonly"
-        :style="{display: checkAllDisplay}"
+        :style="{ display: checkAllDisplay }"
       >
         <slot name="check-all">
-          <u-text text='全选'></u-text>
+          <u-text text="全选"></u-text>
         </slot>
       </u-checkbox>
       <u-checkbox
@@ -63,7 +63,7 @@ export default {
   },
   data() {
     return {
-      currentValue: this.value,
+      currentValue: null,
       itemVMs: [],
       all: false,
     };
@@ -81,10 +81,38 @@ export default {
     },
     itemVMs() {
       this.watchValue(this.value);
+      // 不开启全选就直接return
+      if (!this.checkAll) return;
+      if (this.isCheckAll) {
+        this.all = true;
+        // 这里不能这样判断：this.currentValue.length === 0，这里子项全部为false不代表currentValue.length === 0
+      } else if (
+        !this.currentValue.some((item) =>
+          this.itemVMsExcludesCheckAll.some((vm) => vm.label === item)
+        )
+      ) {
+        this.all = false;
+      } else {
+        // 半选兜底
+        this.all = null;
+      }
+    },
+  },
+  computed: {
+    // 除了全选checkbox以外的子实例
+    itemVMsExcludesCheckAll() {
+      return this.itemVMs.filter((vm) => vm.label !== "check-all");
+    },
+    // 是否被全选
+    isCheckAll() {
+      // 这样判断是为了兼容用户传入的value要比静态设置的子项数目多的场景
+      return this.itemVMsExcludesCheckAll.every((vm) =>
+        this.currentValue.includes(vm.label)
+      );
     },
   },
   mounted() {
-    // 修复bug：多选组，多选项值设为true，如果不操作拿不到值 http://projectmanage.netease-official.lcap.163yun.com/dashboard/DebtDetail?id=2577483154345216
+    // 修复bug：多选组，多选项值设为true，如果不操作拿不到值
     this.initSyncValue(this.value);
     this.watchValue(this.currentValue);
   },
@@ -93,9 +121,9 @@ export default {
       if (value) {
         if (this.converter) value = this.currentConverter.set(value);
         this.currentValue = value;
-        this.itemVMs.forEach(
-          (itemVM) => (itemVM.currentValue = value.includes(itemVM.label))
-        );
+        this.itemVMs.forEach((itemVM) => {
+          itemVM.currentValue = value.includes(itemVM.label);
+        });
       } else {
         const value = [];
         this.itemVMs.forEach(
@@ -116,41 +144,48 @@ export default {
         const length = this.currentValue.length - 1;
         return length >= this.min;
       }
+      return true;
+    },
+    toggleCheckAll(all) {
+      this.all = all;
+      if (all) {
+        this.itemVMsExcludesCheckAll.forEach((vm) => {
+          if (!this.currentValue.includes(vm.label)) {
+            this.currentValue.push(vm.label);
+          }
+        });
+      } else {
+        this.itemVMsExcludesCheckAll.forEach(({ label }) => {
+          const index = this.currentValue.indexOf(label);
+          if (index === -1) return;
+          this.currentValue.splice(index, 1);
+        });
+      }
+    },
+    togglePrimaryCheckBox(value, label) {
+      if (value && !this.currentValue.includes(label)) {
+        this.currentValue.push(label);
+      } else if (!value && this.currentValue.includes(label)) {
+        this.currentValue.splice(this.currentValue.indexOf(label), 1);
+      }
+      // 先给默认值兜底，下一帧更新成正确的数据，保证全选状态的正确性
+      this.all = false;
+      this.$nextTick(() => {
+        this.all = this.itemVMsExcludesCheckAll.some((vm) => vm.currentValue)
+          ? null
+          : false;
+        if (this.isCheckAll) {
+          this.all = true;
+        }
+      });
     },
     onCheck($event) {
       const value = $event.value;
       const label = $event.itemVM.label;
       if (label !== "check-all") {
-        if (value && !this.currentValue.includes(label)) {
-          this.currentValue.push(label);
-        } else if (!value && this.currentValue.includes(label)) {
-          this.currentValue.splice(this.currentValue.indexOf(label), 1);
-        }
-        this.all = false;
-        this.$nextTick(() => {
-          this.all = this.currentValue.length === 0 ? false : null;
-          if (
-            !this.all &&
-            value &&
-            this.currentValue.length === this.currentDataSource.data.length
-          ) {
-            this.all = true;
-          }
-        });
-        console.log("this.currentValue", this.all);
+        this.togglePrimaryCheckBox(value, label);
       } else {
-        this.all = value;
-        this.itemVMs.forEach((vm) => {
-          if (
-            !this.currentValue.includes(vm.label) &&
-            vm.label !== "check-all" &&
-            value
-          ) {
-            this.currentValue.push(vm.label);
-          } else {
-            this.currentValue = [];
-          }
-        });
+        this.toggleCheckAll(value);
       }
 
       let currentValue = this.currentValue;
@@ -183,7 +218,6 @@ export default {
         // 有可能使用方在使用组件的时候，初始双向绑定的value值是空的，等接口返回后去设置value值。有些在使用该组件的时候会监听input方法，这里抛出事件后会导致使用方的值变空
         // 所有如果得到的值和value值一样（可能都是空），就不抛出事件
         if (JSON.stringify(currentValue) !== JSON.stringify(value)) {
-          console.log("input emit");
           this.$emit("input", currentValue);
           this.$emit("update:value", currentValue);
         }
