0272e702907bc0419e794e2ed6fbf4bad52ac040
姜令浩 <jianglinghao@corp.netease.com>
2023-11-03 16:48:14 +0800

    feat: 精度取最大值

--- src/components/u-number-input.vue/index.vue
+++ src/components/u-number-input.vue/index.vue
@@ -240,7 +240,9 @@ export default {
                 // 平台long类型 最大精度20
                 CoDecimal = Decimal.clone({ precision: 20 });
             } else {
-                const rounding = this.decimalLength + 1;
+                const valueLen = String(this.value).length;
+                const len = Math.max(valueLen, this.decimalLength);
+                const rounding = len + 1;
                 CoDecimal = Decimal.clone({ precision: rounding * 2 });
             }
             return CoDecimal;
