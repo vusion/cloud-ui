eabb4bc9230397dc21f988f833b7edeed9d16820
御风 <18012261618@126.com>
2023-11-06 14:11:43 +0800

    feat: 修改目录名

--- src/mixins/i18n/index.js
+++ src/mixins/i18n/index.js
@@ -2,16 +2,16 @@ import camelCase from 'lodash/camelCase';
 import get from 'lodash/get';
 import template from 'lodash/template';
 
-import locale from '../../i18n';
+import locale from '../../locale';
 
 export default function (name) {
     return {
         // 在注册props之前，往vm上挂载一个$tt方法
         beforeCreate() {
             this.$tt = function (key, data) {
-                const k = `${camelCase(name)}_${key}`;
+                const scopeKey = `${camelCase(name)}_${key}`;
                 const messages = locale.messages();
-                const message = get(messages, k) || get(messages, key);
+                const message = get(messages, scopeKey) || get(messages, key);
 
                 return template(message, {
                     interpolate: /\{(.+?)\}/g,
