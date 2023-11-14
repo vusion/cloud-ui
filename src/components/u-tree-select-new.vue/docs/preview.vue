99ad50dc9cdb9a7eeb8a9a4cf1381cf8ec35c4ed
violarong <zxrong@gmail.com>
2023-11-14 17:16:00 +0800

    feat UTreeSelectNew 开放IDE主题样式设置

---
+++ src/components/u-tree-select-new.vue/docs/preview.vue
@@ -0,0 +1,31 @@
+<template>
+    <u-tree-select-new :data-source="list" text-field="title" value-field="title">
+    </u-tree-select-new>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            list: [
+                { text1: '节点 1', children: [
+                    { text1: '节点 1.1' },
+                    { text1: '节点 1.2', children: [
+                        { text1: '节点 1.2.1' },
+                        { text1: '节点 1.2.2' },
+                    ] },
+                    { text1: '节点 1.3' },
+                    { text1: '节点 1.4' },
+                ] },
+                { text1: '节点 2' },
+                { text1: '节点 3', children: [
+                    { text1: '节点 3.1' },
+                    { text1: '节点 3.2' },
+                ] },
+            ],
+        };
+    },
+};
+</script>
+
