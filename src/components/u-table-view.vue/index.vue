1771994a69a6eca7274e7627543bc702959e1f13
姜令浩 <jianglinghao@corp.netease.com>
2023-11-17 17:48:15 +0800

    feat: 支持包装类 table组件反选

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -1707,12 +1707,12 @@ export default {
         },
         /* Selection Methods */
         watchValue(value) {
-            if (this.selectedItem && this.$at(this.selectedItem, this.valueField) === value)
+            if (this.selectedItem && (this.$at(this.selectedItem, this.valueField) === value || String(this.$at(this.selectedItem, this.valueField)) === String(value)))
                 return;
             if (value === undefined)
                 this.selectedItem = undefined;
             else {
-                this.selectedItem = this.currentData && this.currentData.find((item) => this.$at(item, this.valueField) === value); // @TODO: Group
+                this.selectedItem = this.currentData && this.currentData.find((item) => this.$at(item, this.valueField) === value || String(this.$at(item, this.valueField)) === String(value)); // @TODO: Group
             }
         },
         watchValues(values) {
@@ -1889,7 +1889,7 @@ export default {
         getCheckedItems() {
             const items = [];
             Object.keys(this.checkedItems).forEach((itemKey) => {
-                const inValues = this.currentValues.find((value) => '' + value === itemKey);
+                const inValues = this.currentValues.find((value) => '' + value === itemKey || String(value) === String(itemKey));
                 if (inValues) {
                     items.push(this.checkedItems[itemKey]);
                 }
@@ -1980,8 +1980,8 @@ export default {
                         this.check(item, item.checked, true);
                     }
                     // 促使currentData更新
-                    const index = this.currentData.findIndex((currentData) => this.$at(currentData, this.valueField) === this.$at(item, this.valueField));
-                    const newDataIndex = this.currentData.findIndex((currentData) => this.$at(currentData, this.valueField) === this.$at(result[0], this.valueField));
+                    const index = this.currentData.findIndex((currentData) => this.$at(currentData, this.valueField) === this.$at(item, this.valueField) || String(this.$at(currentData, this.valueField)) === String(this.$at(item, this.valueField)));
+                    const newDataIndex = this.currentData.findIndex((currentData) => this.$at(currentData, this.valueField) === this.$at(result[0], this.valueField) || String(this.$at(currentData, this.valueField)) === String(this.$at(result[0], this.valueField)));
                     if (index !== -1 && newDataIndex === -1) {
                         const treeData = this.processTreeData(result, item.tableTreeItemLevel + 1, item);
                         this.currentData.splice(index + 1, 0, ...treeData);
@@ -2289,7 +2289,7 @@ export default {
                 } else if (this.draggable) {
                     // 表格内拖拽，sourcePath和targetPath不一样的时候才emit事件
                     // 当元素被移除的时候可能不会触发dragend，结合判断数据是否在table里
-                    const inTable = this.valueField ? dragStartData.sourceData && dragStartData.sourceData.item && !!this.currentData.find((titem) => this.$at(titem, this.valueField) === this.$at(dragStartData.sourceData.item, this.valueField)) : true;
+                    const inTable = this.valueField ? dragStartData.sourceData && dragStartData.sourceData.item && !!this.currentData.find((titem) => this.$at(titem, this.valueField) === this.$at(dragStartData.sourceData.item, this.valueField) || String(this.$at(titem, this.valueField)) === String(this.$at(dragStartData.sourceData.item, this.valueField))) : true;
                     const inTheSameTable = this.dragState.sourcePath !== undefined && inTable;
                     const finalSource = Object.assign({}, dragStartData.sourceData);
                     finalSource.index = this.dragState.targetPath;
@@ -2640,7 +2640,7 @@ export default {
                     for (let i = currentData.length - 1; i >= 0; i--) {
                         const item = currentData[i];
                         const itemValue = this.$at(item, columnVM.field);
-                        if (itemValue === this.$at(currentData[i - 1], columnVM.field)) {
+                        if (itemValue === this.$at(currentData[i - 1], columnVM.field) || String(itemValue) === String(this.$at(currentData[i - 1], columnVM.field))) {
                             if (!this.autoRowSpan[i]) {
                                 this.autoRowSpan[i] = [];
                             }
