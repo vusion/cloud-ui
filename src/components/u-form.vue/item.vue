7c3816f748c8e0570be9cf715928070b3efefb01
周勋 <zhouxun03@corp.netease.com>
2023-12-19 17:36:27 +0800

    feat: 修改preview

--- src/components/u-form.vue/item.vue
+++ src/components/u-form.vue/item.vue
@@ -106,11 +106,11 @@ export default {
                 || (this.rootVM && this.rootVM.labelEllipsis)
             );
         },
-        preview() {
-            return (
-                this.rootVM && this.rootVM.preview
-            );
-        },
+        // preview() {
+        //     return (
+        //         this.rootVM && this.rootVM.preview
+        //     );
+        // },
     },
     watch: {
         span(span) {
--- src/components/u-text.vue/index.vue
+++ src/components/u-text.vue/index.vue
@@ -1,6 +1,6 @@
 <template>
 <span :class="$style.root" v-on="$listeners" vusion-slot-name-edit="text">
-    <slot>{{ text ? text : '--' }}</slot>
+    <slot>{{ text }}</slot>
 </span>
 </template>
 
