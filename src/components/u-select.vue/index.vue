c4edfae16d69d26c9c5cc14c30c194b3d9d7b8ef
violarong <zxrong@gmail.com>
2023-12-25 09:53:41 +0800

    :bug: 选择器筛选的时候，选择过滤值后无法通过enter 选中

--- src/components/u-select.vue/index.vue
+++ src/components/u-select.vue/index.vue
@@ -663,7 +663,7 @@ export default {
         },
         onEnter(event) {
             // 当footer里的输入框按enter的时候，阻止行为
-            if (this.$refs.footer.contains(event.target))
+            if (this.$refs.footer && this.$refs.footer.contains(event.target))
                 return;
             if (this.focusedVM)
                 this.select(this.focusedVM);
@@ -817,7 +817,7 @@ export default {
         },
         onDelete(event) {
             // 当footer里的输入框按delete的时候，阻止行为，不然弹层会关闭
-            if (this.$refs.footer.contains(event.target))
+            if (this.$refs.footer && this.$refs.footer.contains(event.target))
                 return;
             if (this.clearable) {
                 this.clear();
