e31b5e3a160de07c9a78428e6846f485f613cf33
changpei <changpei01@corp.netease.com>
2024-03-18 16:09:23 +0800

    fix: selectTree代码迁移

--- src/components/u-tree-select-new.vue/index.vue
+++ src/components/u-tree-select-new.vue/index.vue
@@ -84,6 +84,7 @@
             :style="getPopperStyle()">
             <!-- 目前只支持单选 -->
             <u-tree-view-new v-if="popperOpened" ref="treeView"
+                :renderOptimize="renderOptimize"
                 :if-expanded="ifExpanded"
                 style="border: none; min-width: 100%; display: inline-block"
                 :value="value"
@@ -187,6 +188,7 @@ export default {
         },
         filterFields: { type: Array, default: () => ['text'] },
         ifExpanded: { type: Boolean, default: false },
+        renderOptimize: { type: Boolean, default: false },
     },
     data() {
         return {
