9c1002948e85ab8872887f0c0a103c6ef00c0307
刘宇阳 <myronliu347@gmail.com>
2024-04-19 20:00:43 +0800

    fix(Uploader): 修复drag 模式 card 展示没有校验信息的问题

--- src/components/u-uploader.vue/index.vue
+++ src/components/u-uploader.vue/index.vue
@@ -18,7 +18,7 @@
         <input :class="$style.file" ref="file" type="file" :name="name" :accept="accept" :multiple="multiple" :readonly="readonly" :disabled="disabled" @click.stop @change="onChange">
         <slot></slot>
     </div>
-    <template v-if="listType !== 'card'">
+    <template v-if="listType !== 'card' || (uploadEnable && draggable && !readonly)">
         <div v-if="description" :class="$style.description">{{ description }}</div>
         <f-scroll-view trigger="hover" v-if="showErrorMessage && errorMessage.length">
             <div :class="$style.errwrap">
