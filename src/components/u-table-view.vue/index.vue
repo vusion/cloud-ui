d6a8608258e380ff28a9a04d72a0ed8456332fde
violarong <zxrong@gmail.com>
2024-04-26 13:36:59 +0800

    fix(TableView): loadTo方法page增加空字符串、null处理

--- src/components/u-table-view.vue/index.vue
+++ src/components/u-table-view.vue/index.vue
@@ -3277,7 +3277,7 @@ export default {
             this.syncHeadScroll();
         },
         loadTo(page) {
-            if (page !== undefined) {
+            if (!['', null, undefined].includes(page)) {
                 this.page(page);
             } else {
                 this.load();
